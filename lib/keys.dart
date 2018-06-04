8459b3d7d8e3e2e40e5295985e676ab990e2af66
unknown <hillelcoren@gmail.com>
2018-06-04 06:08:57 -0700

    Refactor

--- lib/keys.dart
+++ lib/keys.dart
@@ -5,8 +5,8 @@ class NinjaKeys {
   static final dashboard = const Key('__dashboard__');
   static final loginScreen = const Key('__login_screen__');
   static final clientList = const Key('__client_list__');
-  static final productHome = const Key('__product_home__');
 
+  static final productHome = const Key('__product_home__');
   static final productList = const Key('__product_list__');
   static final productItem = (int id) => Key('__product_item_${id}__');
 }
