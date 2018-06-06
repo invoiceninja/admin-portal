6a5b77e8acb6ba0292429dbe06467ef8bcf50fa5
unknown <hillelcoren@gmail.com>
2018-06-06 06:09:07 -0700

    Working on clients

--- lib/redux/company/company_reducer.dart
+++ lib/redux/company/company_reducer.dart
@@ -2,12 +2,14 @@ import 'package:redux/redux.dart';
 import 'package:invoiceninja/data/models/models.dart';
 import 'package:invoiceninja/redux/company/company_state.dart';
 import 'package:invoiceninja/redux/product/product_reducer.dart';
+import 'package:invoiceninja/redux/client/client_reducer.dart';
 import 'package:invoiceninja/redux/dashboard/dashboard_reducer.dart';
 import 'package:invoiceninja/redux/company/company_actions.dart';
 
 CompanyState companyReducer(CompanyState state, action) {
 
   return state.rebuild((b) => b
+      ..clientState.replace(clientsReducer(state.clientState, action))
       ..company.replace(companyEntityReducer(state.company, action))
       ..dashboardState.replace(dashboardReducer(state.dashboardState, action))
       ..productState.replace(productsReducer(state.productState, action))
