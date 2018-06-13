567b8544c91b7894dec6839823acfc27dae19942
unknown <hillelcoren@gmail.com>
2018-06-13 12:54:54 -0700

    Change bgcolor is search is set

--- lib/ui/app/app_search.dart
+++ lib/ui/app/app_search.dart
@@ -7,7 +7,6 @@ import 'package:invoiceninja/utils/localization.dart';
 import 'package:redux/redux.dart';
 
 class AppSearch extends StatelessWidget {
-
   final EntityType entityType;
   final String search;
   final Function(String) onSearchChanged;
@@ -23,31 +22,35 @@ class AppSearch extends StatelessWidget {
     var localization = AppLocalization.of(context);
 
     return StoreConnector<AppState, ListUIState>(
-      converter: (Store<AppState> store) => store.state.getListState(entityType),
+      converter: (Store<AppState> store) =>
+          store.state.getListState(entityType),
       builder: (BuildContext context, listUIState) {
         return listUIState.search == null
             ? Text(localization.lookup(entityType.toString()))
             : Container(
-          padding: const EdgeInsets.only(left: 8.0),
-          height: 38.0,
-          margin: EdgeInsets.only(bottom: 2.0),
-          decoration: BoxDecoration(
-              color: Colors.grey[200],
-              border: Border.all(color: Colors.grey[700], width: 1.0),
-              borderRadius: BorderRadius.circular(6.0)),
-          child: TextField(
-            decoration: InputDecoration(
-              prefixIcon: Padding(
-                padding: EdgeInsets.only(right: 8.0),
-                  child: Icon(Icons.search),
-              ),
-                border: InputBorder.none,
-                hintText: localization.search),
-            autofocus: true,
-            autocorrect: false,
-            onChanged: (value) => onSearchChanged(value),
-          ),
-        );
+                padding: const EdgeInsets.only(left: 8.0),
+                height: 38.0,
+                margin: EdgeInsets.only(bottom: 2.0),
+                decoration: BoxDecoration(
+                    color: listUIState.search != null &&
+                            listUIState.search.isNotEmpty
+                        ? Colors.yellow[200]
+                        : Colors.grey[100],
+                    border: Border.all(color: Colors.grey[400], width: 1.0),
+                    borderRadius: BorderRadius.circular(6.0)),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(right: 8.0),
+                        child: Icon(Icons.search),
+                      ),
+                      border: InputBorder.none,
+                      hintText: localization.search),
+                  autofocus: true,
+                  autocorrect: false,
+                  onChanged: (value) => onSearchChanged(value),
+                ),
+              );
       },
     );
   }
