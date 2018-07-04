23684fad16744d3bbb862679c91d76375d44aa23
unknown <hillelcoren@gmail.com>
2018-07-04 08:28:40 +0300

    Refactor

--- lib/data/models/invoice_model.dart
+++ lib/data/models/invoice_model.dart
@@ -2,6 +2,7 @@ import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 import 'package:invoiceninja/data/models/entities.dart';
+import 'package:invoiceninja/data/models/mixins/invoice_mixin.dart';
 import 'package:invoiceninja/redux/app/app_state.dart';
 import 'package:invoiceninja/utils/formatting.dart';
 
