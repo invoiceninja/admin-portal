ca2de491257c6340d446111f2d7747e4941d9e71
Hillel Coren <hillelcoren@gmail.com>
2018-08-19 15:25:44 -0700

    Match master

--- lib/redux/product/product_middleware.dart
+++ lib/redux/product/product_middleware.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceninja_flutter/data/models/models.dart';
-import 'package:invoiceninja_flutter/redux/invoice/invoice_actions.dart';
 import 'package:invoiceninja_flutter/redux/ui/ui_actions.dart';
+import 'package:invoiceninja_flutter/redux/invoice/invoice_actions.dart';
 import 'package:invoiceninja_flutter/ui/product/edit/product_edit_vm.dart';
 import 'package:invoiceninja_flutter/ui/product/product_screen.dart';
 import 'package:redux/redux.dart';
