8ce53573b1d49b208ef17c4c3096d1b728481e63
Hillel Coren <hillelcoren@gmail.com>
2018-08-26 12:50:49 -0700

    Quotes

---
+++ lib/ui/quote/quote_email_vm.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:invoiceninja_flutter/redux/client/client_actions.dart';
+import 'package:invoiceninja_flutter/redux/quote/quote_actions.dart';
+import 'package:invoiceninja_flutter/ui/app/invoice/invoice_email_view.dart';
+import 'package:invoiceninja_flutter/ui/invoice/invoice_email_vm.dart';
+import 'package:invoiceninja_flutter/utils/completers.dart';
+import 'package:redux/redux.dart';
+import 'package:invoiceninja_flutter/data/models/models.dart';
+import 'package:invoiceninja_flutter/redux/app/app_state.dart';
+
+class QuoteEmailScreen extends StatelessWidget {
+  static const String route = '/quote/email';
+
+  const QuoteEmailScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, EmailInvoiceVM>(
+      onInit: (Store<AppState> store) {
+        final state = store.state;
+        final quoteId = state.uiState.quoteUIState.selectedId;
+        final quote = state.quoteState.map[quoteId];
+        final client = state.clientState.map[quote.clientId];
+        if (client.areActivitiesStale) {
+          store.dispatch(LoadClient(clientId: client.id, loadActivities: true));
+        }
+      },
+      converter: (Store<AppState> store) {
+        final state = store.state;
+        final quoteId = state.uiState.quoteUIState.selectedId;
+        final quote = state.quoteState.map[quoteId];
+        return EmailInvoiceVM.fromStore(store, quote);
+      },
+      builder: (context, viewModel) {
+        return InvoiceEmailView(
+          viewModel: viewModel,
+        );
+      },
+    );
+  }
+}
+
+class EmailQuoteVM extends EmailEntityVM{
+
+  EmailQuoteVM({
+    bool isLoading,
+    bool isSaving,
+    CompanyEntity company,
+    InvoiceEntity invoice,
+    ClientEntity client,
+    Function(BuildContext, EmailTemplate, String, String) onSendPressed,
+  }): super(
+    isLoading: isLoading,
+    isSaving: isSaving,
+    company: company,
+    invoice: invoice,
+    client: client,
+    onSendPressed: onSendPressed,
+  );
+
+  factory EmailQuoteVM.fromStore(
+      Store<AppState> store, InvoiceEntity quote) {
+    final state = store.state;
+
+    return EmailQuoteVM(
+        isLoading: state.isLoading,
+        isSaving: state.isSaving,
+        company: state.selectedCompany,
+        invoice: quote,
+        client: state.clientState.map[quote.clientId],
+        onSendPressed: (context, template, subject, body) =>
+            store.dispatch(EmailQuoteRequest(
+              completer: popCompleter(context, true),
+              quoteId: quote.id,
+              template: template,
+              subject: subject,
+              body: body,
+            )));
+  }
+}
