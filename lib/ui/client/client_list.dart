5813c242fbb7503e40419c6b75490ec7df040103
Hillel Coren <hillelcoren@gmail.com>
2018-09-06 22:33:04 -0700

    Add ‘new invoice’ options to client list

--- lib/ui/client/client_list.dart
+++ lib/ui/client/client_list.dart
@@ -37,30 +37,53 @@ class ClientList extends StatelessWidget {
     return _buildListView(context);
   }
 
-  void _showMenu(BuildContext context, ClientEntity client) async {
+  void _showMenu(
+      BuildContext context, ClientEntity client, UserEntity user) async {
     if (client == null) {
       return;
     }
     final user = viewModel.user;
     final message = await showDialog<String>(
         context: context,
-        builder: (BuildContext dialogContext) => SimpleDialog(
-                children:
-                    client.getEntityActions(user: user).map((entityAction) {
-              if (entityAction == null) {
-                return Divider();
-              } else {
-                return ListTile(
-                  leading: Icon(getEntityActionIcon(entityAction)),
-                  title: Text(AppLocalization.of(context)
-                      .lookup(entityAction.toString())),
-                  onTap: () {
-                    Navigator.of(dialogContext).pop();
-                    viewModel.onEntityAction(context, client, entityAction);
-                  },
-                );
-              }
-            }).toList()));
+        builder: (BuildContext dialogContext) {
+          final actions = <Widget>[];
+          if (user.canCreate(EntityType.invoice)) {
+            actions.add(EntityActionListTile(
+              client: client,
+              entityAction: EntityAction.invoice,
+              mainContext: context,
+              viewModel: viewModel,
+            ));
+          }
+          if (user.canCreate(EntityType.payment)) {
+            actions.add(EntityActionListTile(
+              client: client,
+              entityAction: EntityAction.payment,
+              mainContext: context,
+              viewModel: viewModel,
+            ));
+          }
+
+          if (actions.isNotEmpty) {
+            actions.add(Divider());
+          }
+
+          actions
+              .addAll(client.getEntityActions(user: user).map((entityAction) {
+            if (entityAction == null) {
+              return Divider();
+            } else {
+              return EntityActionListTile(
+                client: client,
+                entityAction: entityAction,
+                mainContext: context,
+                viewModel: viewModel,
+              );
+            }
+          }).toList());
+
+          return SimpleDialog(children: actions);
+        });
 
     if (message != null) {
       Scaffold.of(context).showSnackBar(SnackBar(
@@ -85,19 +108,43 @@ class ClientList extends StatelessWidget {
                 client: client,
                 onEntityAction: (EntityAction action) {
                   if (action == EntityAction.more) {
-                    _showMenu(context, client);
+                    _showMenu(context, client, viewModel.user);
                   } else {
                     viewModel.onEntityAction(context, client, action);
                   }
                 },
                 onTap: () => viewModel.onClientTap(context, client),
-                onLongPress: () => _showMenu(context, client),
+                onLongPress: () => _showMenu(context, client, viewModel.user),
               ),
               Divider(
                 height: 1.0,
               ),
             ]);
           }),
+    );
+  }
+}
+
+class EntityActionListTile extends StatelessWidget {
+  const EntityActionListTile(
+      {this.client, this.entityAction, this.viewModel, this.mainContext});
+
+  final ClientEntity client;
+  final EntityAction entityAction;
+  final ClientListVM viewModel;
+  final BuildContext mainContext;
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalization.of(context);
+
+    return ListTile(
+      leading: Icon(getEntityActionIcon(entityAction)),
+      title: Text(localization.lookup(entityAction.toString())),
+      onTap: () {
+        Navigator.of(context).pop();
+        viewModel.onEntityAction(mainContext, client, entityAction);
+      },
     );
   }
 }
