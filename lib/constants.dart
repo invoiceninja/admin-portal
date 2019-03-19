a8dfb569b5750d8a1884849cb113cda4c4f1a068
Hillel Coren <hillelcoren@gmail.com>
2019-03-19 07:48:40 +0200

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // This version must be updated in tandem with the pubspec version.
-const String kAppVersion = '0.1.38';
+const String kAppVersion = '0.1.39';
 const String kAppUrl = 'https://app.invoiceninja.com';
 
 const String kAppleStoreUrl =
