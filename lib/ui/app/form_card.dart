d9d7b49ca78996e0372ba758f9fe8cf2edc5244a
Hillel Coren <hillelcoren@gmail.com>
2019-05-30 18:04:58 +0300

    Code gen

--- lib/ui/app/form_card.dart
+++ lib/ui/app/form_card.dart
@@ -17,8 +17,13 @@ class FormCard extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.only(
               left: 16.0, top: 16.0, right: 16.0, bottom: 20.0),
-          child: Column(
-            children: children,
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment:CrossAxisAlignment.center,
+              children: children,
+            ),
           ),
         ),
       ),
