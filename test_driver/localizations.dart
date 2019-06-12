3d1e0670774371a05a3507b78eab2e3c48cb4855
Efthymis Sarmpanis <e.sarbanis@gmail.com>
2019-06-12 20:14:06 +0300

    Extract all i18n strings into a mixin so that it can be used in tests as well.

---
+++ test_driver/localizations.dart
@@ -0,0 +1,5 @@
+import 'package:invoiceninja_flutter/utils/i18n.dart';
+
+class TestLocalization extends LocaleCodeAware with LocalizationsProvider {
+  TestLocalization(String localeCode) : super(localeCode);
+}
