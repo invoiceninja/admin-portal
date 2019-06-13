5df6813a7dce02bd85bafd1ce8f0b12e854ca427
Efthymis Sarmpanis <e.sarbanis@gmail.com>
2019-06-13 21:21:28 +0300

    Add key to bottom filter button to distinguish it from others in the screen

--- lib/utils/keys.dart
+++ lib/utils/keys.dart
@@ -26,4 +26,11 @@ class ProductKeys {
   static const String productKey = 'productKey';
   static const String notes = 'productNotes';
   static const String cost = 'productCost';
+  static const String filter = 'productFilter';
+}
+
+class SettingKeys {
+  static const String screen = 'SettingsScreen';
+  static const String drawer = 'settingsDrawer';
+  static const String logoutButton = 'Log Out';
 }
