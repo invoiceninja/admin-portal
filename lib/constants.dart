10a84bbb6791457ff29e64f46ae96ba34aa89ef7
Hillel Coren <hillelcoren@gmail.com>
2019-09-01 21:41:33 +0300

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // This version must be updated in tandem with the pubspec version.
-const String kAppVersion = '0.1.49';
+const String kAppVersion = '0.1.50';
 const String kSiteUrl = 'https://invoiceninja.com';
 const String kAppUrl = 'https://app.invoiceninja.com';
 const String kPrivacyPolicyURL = 'https://www.invoiceninja.com/privacy-policy';
