f8d1a869157b9cddff58320fab7afc4c25154c28
Hillel Coren <hillelcoren@gmail.com>
2019-10-04 16:56:54 +0300

    Settings

--- lib/ui/settings/settings_list.dart
+++ lib/ui/settings/settings_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:invoiceninja_flutter/constants.dart';
 import 'package:invoiceninja_flutter/ui/app/lists/selected_indicator.dart';
 import 'package:invoiceninja_flutter/ui/settings/settings_list_vm.dart';
@@ -30,38 +31,47 @@ class SettingsList extends StatelessWidget {
         SettingsListTile(
           section: kSettingsCompanyDetails,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.building,
         ),
         SettingsListTile(
           section: kSettingsUserDetails,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.user,
         ),
         SettingsListTile(
           section: kSettingsLocalization,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.globe,
         ),
         SettingsListTile(
           section: kSettingsOnlinePayments,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.creditCard,
         ),
         SettingsListTile(
           section: kSettingsTaxRates,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.percent,
         ),
         SettingsListTile(
           section: kSettingsProducts,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.cube,
         ),
         SettingsListTile(
           section: kSettingsNotifications,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.bell,
         ),
         SettingsListTile(
           section: kSettingsImportExport,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.fileExport,
         ),
         SettingsListTile(
           section: kSettingsDeviceSettings,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.cogs,
         ),
         Container(
           color: Theme.of(context).backgroundColor,
@@ -74,35 +84,43 @@ class SettingsList extends StatelessWidget {
         SettingsListTile(
           section: kSettingsInvoiceSettings,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.fileInvoice,
         ),
         SettingsListTile(
           section: kSettingsInvoiceDesign,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.paintBrush,
         ),
         SettingsListTile(
           section: kSettingsClientPortal,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.cloud,
         ),
         SettingsListTile(
           section: kSettingsBuyNowButtons,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.link,
         ),
         SettingsListTile(
           section: kSettingsEmailSettings,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.solidEnvelope,
         ),
         SettingsListTile(
           section: kSettingsTemplatesAndReminders,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.solidClock,
         ),
         /*
         SettingsListTile(
           section: kSettingsCreditCardsAndBanks,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.link,
         ),
         SettingsListTile(
           section: kSettingsDataVisualizations,
           viewModel: viewModel,
+          icon: FontAwesomeIcons.link,
         ),
          */
       ],
@@ -114,10 +132,12 @@ class SettingsListTile extends StatelessWidget {
   const SettingsListTile({
     @required this.section,
     @required this.viewModel,
+    this.icon,
   });
 
   final String section;
   final SettingsListVM viewModel;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
@@ -126,6 +146,7 @@ class SettingsListTile extends StatelessWidget {
     return SelectedIndicator(
       isSelected: viewModel.state.uiState.containsRoute('/$section'),
       child: ListTile(
+        leading: Icon(icon),
         title: Text(localization.lookup(section)),
         onTap: () {
           viewModel.loadSection(context, section);
