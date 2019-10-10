0c3d059d0f3095b659f92519e3ec7ea05991e1a3
Hillel Coren <hillelcoren@gmail.com>
2019-10-10 09:12:59 +0300

    Fix send email popup on tablets

--- lib/ui/client/edit/client_edit_billing_address.dart
+++ lib/ui/client/edit/client_edit_billing_address.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:invoiceninja_flutter/data/models/entities.dart';
 import 'package:invoiceninja_flutter/ui/app/buttons/elevated_button.dart';
 import 'package:invoiceninja_flutter/ui/app/entity_dropdown.dart';
