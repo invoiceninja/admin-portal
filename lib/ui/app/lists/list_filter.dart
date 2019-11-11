c855335498d8c46df4aae7eabc0b69a18f4470f3
Hillel Coren <hillelcoren@gmail.com>
2019-11-11 22:25:22 +0200

    Settings

--- lib/ui/app/lists/list_filter.dart
+++ lib/ui/app/lists/list_filter.dart
@@ -11,7 +11,7 @@ class ListFilterMessage extends StatelessWidget {
     @required this.onPressed,
     @required this.onClearPressed,
   });
-  
+
   final String filterEntityId;
   final EntityType filterEntityType;
   final Function(BuildContext) onPressed;
@@ -19,50 +19,30 @@ class ListFilterMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final localization = AppLocalization.of(context);
     final state = StoreProvider.of<AppState>(context).state;
 
-    BaseEntity filteredEntity;
+    final filteredEntity = state.getEntityMap(filterEntityType)[filterEntityId];
     String filteredMessage;
 
-    
     switch (filterEntityType) {
       case EntityType.group:
         filteredMessage = localization.filteredByGroup;
-        filteredEntity = filterEntityId != null
-            ? state.groupState.map[filterEntityId]
-            : null;
         break;
       case EntityType.invoice:
         filteredMessage = localization.filteredByInvoice;
-        filteredEntity = filterEntityId != null
-            ? state.invoiceState.map[filterEntityId]
-            : null;
         break;
       case EntityType.client:
         filteredMessage = localization.filteredByClient;
-        filteredEntity = filterEntityId != null
-            ? state.clientState.map[filterEntityId]
-            : null;
         break;
       case EntityType.user:
         filteredMessage = localization.filteredByUser;
-        filteredEntity = filterEntityId != null
-            ? state.userState.map[filterEntityId]
-            : null;
         break;
       case EntityType.project:
         filteredMessage = localization.filteredByProject;
-        filteredEntity = filterEntityId != null
-            ? state.projectState.map[filterEntityId]
-            : null;
         break;
       case EntityType.vendor:
         filteredMessage = localization.filteredByVendor;
-        filteredEntity = filterEntityId != null
-            ? state.vendorState.map[filterEntityId]
-            : null;
         break;
     }
 
@@ -76,7 +56,7 @@ class ListFilterMessage extends StatelessWidget {
             SizedBox(width: 18.0),
             Expanded(
               child: Text(
-                '$filteredMessage: ${filteredEntity.listDisplayName}',
+                '$filteredMessage: ${filteredEntity?.listDisplayName ?? ''}',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 16.0,
