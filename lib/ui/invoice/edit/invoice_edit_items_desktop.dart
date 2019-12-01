20480de8488853aebcf501138ad1a656180c3887
Hillel Coren <hillelcoren@gmail.com>
2019-12-01 20:02:09 +0200

    Settings

--- lib/ui/invoice/edit/invoice_edit_items_desktop.dart
+++ lib/ui/invoice/edit/invoice_edit_items_desktop.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:invoiceninja_flutter/ui/app/form_card.dart';
 import 'package:invoiceninja_flutter/ui/invoice/edit/invoice_edit_items_vm.dart';
+import 'package:invoiceninja_flutter/utils/formatting.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
 
 class InvoiceEditItemsDesktop extends StatelessWidget {
@@ -12,37 +14,7 @@ class InvoiceEditItemsDesktop extends StatelessWidget {
     final localization = AppLocalization.of(context);
     final invoice = viewModel.invoice;
 
-    return DataTable(
-      columns: [
-        DataColumn(
-          label: Text(localization.item),
-        ),
-        DataColumn(
-          label: Text(localization.description),
-        ),
-        DataColumn(
-          label: Text(localization.unitCost),
-        ),
-        DataColumn(
-          label: Text(localization.quantity),
-        ),
-        DataColumn(
-          label: Text(localization.lineTotal),
-        ),
-      ],
-      rows: invoice.lineItems.map((item) => DataRow(
-        cells: [
-          DataCell(Text(item.productKey)),
-          DataCell(Text('')),
-          DataCell(Text('')),
-          DataCell(Text('')),
-          DataCell(Text('')),
-        ],
-      )).toList(),
-    );
-
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
+    return FormCard(
       child: DataTable(
         columns: [
           DataColumn(
@@ -53,23 +25,37 @@ class InvoiceEditItemsDesktop extends StatelessWidget {
           ),
           DataColumn(
             label: Text(localization.unitCost),
+            numeric: true,
           ),
           DataColumn(
             label: Text(localization.quantity),
+            numeric: true,
           ),
           DataColumn(
             label: Text(localization.lineTotal),
           ),
         ],
-        rows: invoice.lineItems.map((item) => DataRow(
-          cells: [
-            DataCell(Text(item.productKey)),
-            DataCell(Text('')),
-            DataCell(Text('')),
-            DataCell(Text('')),
-            DataCell(Text('')),
-          ],
-        )).toList(),
+        rows: invoice.lineItems
+            .map((item) => DataRow(
+                  cells: [
+                    DataCell(TextFormField(
+                      initialValue: item.productKey,
+                    )),
+                    DataCell(TextFormField(
+                      initialValue: item.notes,
+                    )),
+                    DataCell(TextFormField(
+                      initialValue: formatNumber(item.cost, context,
+                          formatNumberType: FormatNumberType.input),
+                    )),
+                    DataCell(TextFormField(
+                      initialValue: formatNumber(item.quantity, context,
+                          formatNumberType: FormatNumberType.input),
+                    )),
+                    DataCell(Text(formatNumber(item.total, context))),
+                  ],
+                ))
+            .toList(),
       ),
     );
   }
