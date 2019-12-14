0807099c790e9f237b9bd3abf2a2bfe1600b48cc
Hillel Coren <hillelcoren@gmail.com>
2019-12-14 22:25:50 +0200

    Payments

--- lib/redux/app/app_state.dart
+++ lib/redux/app/app_state.dart
@@ -385,7 +385,6 @@ abstract class AppState implements Built<AppState, AppStateBuilder> {
     //return 'Fields: ${uiState.settingsUIState.company.customFields} - ${company.customFields}';
     //return 'Custom: ${uiState.settingsUIState.company.settings.customValue1} - ${company.settings.customValue1}';
     //return 'Platform: ${userCompany.token.token}';
-    return 'INVOICE: ${uiState.invoiceUIState.editing.lineItems}';
     return 'Route: ${uiState.currentRoute} Prev: ${uiState.previousRoute}';
   }
 }
