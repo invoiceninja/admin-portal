cf8b4be6d60740f242a29bdef3d6143af5a09643
Hillel Coren <hillelcoren@gmail.com>
2020-01-16 16:51:34 +0200

    Datatables

--- lib/ui/app/presenters/invoice_presenter.dart
+++ lib/ui/app/presenters/invoice_presenter.dart
@@ -3,12 +3,17 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:invoiceninja_flutter/data/models/models.dart';
 import 'package:invoiceninja_flutter/redux/app/app_state.dart';
 import 'package:invoiceninja_flutter/ui/app/presenters/entity_presenter.dart';
+import 'package:invoiceninja_flutter/utils/formatting.dart';
 
 class InvoicePresenter extends EntityPresenter {
   static List<String> getTableFields(UserCompanyEntity userCompany) {
     return [
       InvoiceFields.invoiceNumber,
       InvoiceFields.client,
+      InvoiceFields.invoiceDate,
+      InvoiceFields.amount,
+      InvoiceFields.balance,
+      InvoiceFields.dueDate,
       EntityFields.state,
     ];
   }
@@ -25,6 +30,14 @@ class InvoicePresenter extends EntityPresenter {
         return (state.clientState.map[invoice.clientId] ??
                 ClientEntity(id: invoice.clientId))
             .listDisplayName;
+      case InvoiceFields.invoiceDate:
+        return formatDate(invoice.date, context);
+      case InvoiceFields.amount:
+        return formatNumber(invoice.amount, context);
+      case InvoiceFields.balance:
+        return formatNumber(invoice.balance, context);
+      case InvoiceFields.dueDate:
+        return formatDate(invoice.dueDate, context);
     }
 
     return super.getField(field: field, context: context);
