7c5440949d21a596d1a924313153e6798895a14a
Hillel Coren <hillelcoren@gmail.com>
2020-01-30 12:20:22 +0200

    Legacy fixes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // This version must be updated in tandem with the pubspec version.
-const String kAppVersion = '0.1.53';
+const String kAppVersion = '0.1.54';
 const String kSiteUrl = 'https://invoiceninja.com';
 const String kAppUrl = 'https://app.invoiceninja.com';
 const String kAppPlansURL =
