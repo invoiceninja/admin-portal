54534d55b9c526ca680cd4821e91563d69aa9762
Hillel Coren <hillelcoren@gmail.com>
2020-02-03 18:29:56 +0200

    local_auth fix

--- android/app/src/main/kotlin/com/invoiceninja/client/MainActivity.kt
+++ android/app/src/main/kotlin/com/invoiceninja/client/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.invoiceninja.client
 
 import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import io.flutter.app.FlutterFragmentActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
