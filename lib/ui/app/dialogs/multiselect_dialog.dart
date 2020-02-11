80fb64059ca9bbaee476b2582732d86d30bbfa2e
Hillel Coren <hillelcoren@gmail.com>
2020-02-11 17:16:27 +0200

    Reports

--- lib/ui/app/dialogs/multiselect_dialog.dart
+++ lib/ui/app/dialogs/multiselect_dialog.dart
@@ -14,39 +14,9 @@ void multiselectDialog(
     builder: (BuildContext context) => AlertDialog(
       semanticLabel: title,
       title: Text(title),
-      content: Container(
-        width: double.maxFinite,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Expanded(
-              child: ReorderableListView(
-                children: selected
-                    .map((option) => Padding(
-                          key: ValueKey(option),
-                          padding: const EdgeInsets.all(14),
-                          child: Text(
-                            option,
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                        ))
-                    .toList(),
-                onReorder: (oldIndex, newIndex) {
-                  // https://stackoverflow.com/a/54164333/497368
-                  // These two lines are workarounds for ReorderableListView problems
-                  if (newIndex > selected.length) {
-                    newIndex = selected.length;
-                  }
-                  if (oldIndex < newIndex) {
-                    newIndex--;
-                  }
-
-                  //viewModel.onSortChanged(oldIndex, newIndex);
-                },
-              ),
-            ),
-          ],
-        ),
+      content: _multiselectList(
+        options: options,
+        selected: selected,
       ),
       actions: <Widget>[
         FlatButton(
@@ -63,4 +33,66 @@ void multiselectDialog(
       ],
     ),
   );
+}
+
+class _multiselectList extends StatefulWidget {
+  _multiselectList({this.options, this.selected});
+
+  List<String> options;
+  List<String> selected;
+
+  @override
+  __multiselectListState createState() => __multiselectListState();
+}
+
+class __multiselectListState extends State<_multiselectList> {
+  List<String> selected;
+
+  @override
+  void initState() {
+    super.initState();
+    selected = widget.selected;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Expanded(
+            child: ReorderableListView(
+              children: selected
+                  .map((option) => Padding(
+                        key: ValueKey(option),
+                        padding: const EdgeInsets.all(14),
+                        child: Text(
+                          option,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ))
+                  .toList(),
+              onReorder: (oldIndex, newIndex) {
+                // https://stackoverflow.com/a/54164333/497368
+                // These two lines are workarounds for ReorderableListView problems
+                if (newIndex > selected.length) {
+                  newIndex = selected.length;
+                }
+                if (oldIndex < newIndex) {
+                  newIndex--;
+                }
+
+                final field = selected[oldIndex];
+                selected.remove(field);
+                selected.insert(newIndex, field);
+
+                //viewModel.onSortChanged(oldIndex, newIndex);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
