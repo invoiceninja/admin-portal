93b4b2a3c3ccf97f2094b5a1411cae570d807413
Hillel Coren <hillelcoren@gmail.com>
2020-02-13 20:24:03 +0200

    Reports

--- lib/ui/app/dialogs/multiselect_dialog.dart
+++ lib/ui/app/dialogs/multiselect_dialog.dart
@@ -14,6 +14,7 @@ void multiselectDialog(
 
   showDialog<AlertDialog>(
     context: context,
+    barrierDismissible: false,
     builder: (BuildContext context) => AlertDialog(
       semanticLabel: title,
       title: Text(title),
