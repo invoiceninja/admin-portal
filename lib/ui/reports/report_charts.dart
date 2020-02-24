a752a8155c881b0e397cc4db344a0998f77b05bb
Hillel Coren <hillelcoren@gmail.com>
2020-02-24 17:09:34 +0200

    Reports

---
+++ lib/ui/reports/report_charts.dart
@@ -0,0 +1,117 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'package:invoiceninja_flutter/ui/app/form_card.dart';
+import 'package:invoiceninja_flutter/ui/reports/reports_screen.dart';
+import 'package:invoiceninja_flutter/ui/reports/reports_screen_vm.dart';
+
+class ReportCharts extends StatelessWidget {
+  const ReportCharts({@required this.viewModel});
+
+  final ReportsScreenVM viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    final state = viewModel.state;
+    final reportState = viewModel.reportState;
+
+    if (reportState.chart.isEmpty || reportState.group.isEmpty) {
+      return SizedBox();
+    }
+
+    final color = state.prefState.enableDarkMode
+        ? charts.MaterialPalette.white
+        : charts.MaterialPalette.black;
+
+    final numericAxis = charts.NumericAxisSpec(
+        renderSpec: charts.GridlineRendererSpec(
+            labelStyle: charts.TextStyleSpec(color: color),
+            lineStyle: charts.LineStyleSpec(color: color)));
+
+    final ordinalAxis = charts.OrdinalAxisSpec(
+        renderSpec: charts.GridlineRendererSpec(
+      lineStyle:
+          charts.LineStyleSpec(color: charts.MaterialPalette.transparent),
+      labelStyle: charts.TextStyleSpec(fontSize: 10, color: color),
+      labelRotation: 45,
+    ));
+
+    final dateTimeAxis = charts.DateTimeAxisSpec(
+        renderSpec: charts.SmallTickRendererSpec(
+      labelStyle: charts.TextStyleSpec(color: color),
+      lineStyle: charts.LineStyleSpec(color: color),
+    ));
+
+    Widget child;
+    switch (getReportColumnType(reportState.group, context)) {
+      case ReportColumnType.string:
+      case ReportColumnType.bool:
+        child = charts.BarChart(
+          [
+            charts.Series<dynamic, String>(
+                id: 'chart',
+                colorFn: (dynamic _, __) =>
+                    charts.MaterialPalette.blue.shadeDefault,
+                domainFn: (dynamic item, _) => item['name'],
+                measureFn: (dynamic item, _) => item['value'],
+                data: viewModel.groupTotals.rows
+                    .map((key) => {
+                          'name': key,
+                          'value': viewModel.groupTotals.totals[key]
+                              [reportState.chart]
+                        })
+                    .toList())
+          ],
+          animate: true,
+          primaryMeasureAxis: numericAxis,
+          domainAxis: ordinalAxis,
+        );
+        break;
+      case ReportColumnType.date:
+      case ReportColumnType.dateTime:
+        final keys = viewModel.groupTotals.rows
+            .where((element) => element.isNotEmpty)
+            .toList();
+        keys.sort((String str1, String str2) => str1.compareTo(str2));
+        child = charts.TimeSeriesChart(
+          [
+            charts.Series<dynamic, DateTime>(
+                id: 'chart',
+                colorFn: (dynamic _, __) =>
+                    charts.MaterialPalette.blue.shadeDefault,
+                domainFn: (dynamic item, _) => DateTime.tryParse(item['name']),
+                measureFn: (dynamic item, _) => item['value'],
+                data: keys
+                    .map((key) => {
+                          'name': key,
+                          'value': viewModel.groupTotals.totals[key]
+                              [reportState.chart]
+                        })
+                    .toList())
+          ],
+          animate: true,
+          primaryMeasureAxis: numericAxis,
+          domainAxis: dateTimeAxis,
+          /*
+          behaviors: [
+            charts.SeriesLegend(
+              outsideJustification: charts.OutsideJustification.endDrawArea,
+            )
+          ],
+           */
+        );
+        break;
+      case ReportColumnType.number:
+    }
+
+    return child == null
+        ? SizedBox()
+        : FormCard(
+            child: ClipRect(
+              child: SizedBox(
+                height: 200,
+                child: child,
+              ),
+            ),
+          );
+  }
+}
