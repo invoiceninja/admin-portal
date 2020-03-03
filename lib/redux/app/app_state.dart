0729a5a785e5c64ba803f1dfea526d2b28c99d31
Hillel Coren <hillelcoren@gmail.com>
2020-03-03 16:15:41 +0200

    Debug

--- lib/redux/app/app_state.dart
+++ lib/redux/app/app_state.dart
@@ -486,7 +486,7 @@ abstract class AppState implements Built<AppState, AppStateBuilder> {
     //return 'History: $historyList';
     //return 'Use inclusive: ${invoiceUIState.editing.usesInclusiveTaxes}';
     //return 'Invitations: ${invoiceUIState.editing.invitations}';
-    //return 'Token: ${userCompanyStates.map((state) => state.token.name).where((name) => name.isNotEmpty).toList().join(', ')}';
+    return 'Token: ${userCompanyStates.map((state) => state.token.token).where((name) => name.isNotEmpty).toList().join(', ')}';
     //return 'Settings: ${company.settings.companyLogo}';
     return 'Layout: ${prefState.appLayout}, Route: ${uiState.currentRoute} Prev: ${uiState.previousRoute}';
   }
