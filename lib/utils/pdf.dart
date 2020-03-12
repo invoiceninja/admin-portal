918abbe7920bf2f9cc022a3d41fc1b4a8e6c20de
Hillel Coren <hillelcoren@gmail.com>
2020-03-12 15:11:34 +0200

    PDFs

--- lib/utils/pdf.dart
+++ lib/utils/pdf.dart
@@ -10,9 +10,13 @@ import 'package:invoiceninja_flutter/utils/platforms.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:native_pdf_renderer/native_pdf_renderer.dart';
+import 'package:invoiceninja_flutter/utils/web_stub.dart'
+    if (dart.library.html) 'package:invoiceninja_flutter/utils/web.dart';
 
 Future<Null> viewPdf(InvoiceEntity invoice, BuildContext context) async {
   final localization = AppLocalization.of(context);
+
+  /*
   if (Platform.isIOS) {
     if (await canLaunch(invoice.invitationBorderlessLink)) {
       await launch(invoice.invitationBorderlessLink,
@@ -23,11 +27,17 @@ Future<Null> viewPdf(InvoiceEntity invoice, BuildContext context) async {
 
     return;
   }
+  */
 
   showDialog<Scaffold>(
       context: context,
       builder: (BuildContext context) {
         final localization = AppLocalization.of(context);
+
+        if (kIsWeb) {
+          //registerWebView(widget.html);
+        }
+
         return Scaffold(
           appBar: AppBar(
             title: Text(localization.invoice + ' ' + (invoice.number ?? '')),
@@ -47,60 +57,79 @@ Future<Null> viewPdf(InvoiceEntity invoice, BuildContext context) async {
           body: Container(
             color: Colors.grey,
             padding: const EdgeInsets.symmetric(vertical: 16),
-            child: FutureBuilder(
-                future: renderPDF(context, invoice),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<PDFPageImage>> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                    case ConnectionState.active:
-                    case ConnectionState.waiting:
-                      return LoadingIndicator();
-                    case ConnectionState.done:
-                      if (snapshot.hasError)
-                        return Text(
-                            '${getPdfRequirements(context)} - Error: ${snapshot.error}');
-                      else
-                        return snapshot.data.length == 1
-                            ? Center(
-                                child: Container(
-                                  color: Colors.white,
-                                  child: Image(
-                                      image: MemoryImage(
-                                          snapshot.data.first.bytes),
-                                      height: double.infinity),
-                                ),
-                              )
-                            : ListView(
-                                scrollDirection: Axis.horizontal,
-                                children: snapshot.data
-                                    .map((page) => Row(
-                                          children: <Widget>[
-                                            Container(
-                                              width: 20,
-                                              height: double.infinity,
-                                              color: Colors.grey,
-                                            ),
-                                            Container(
-                                              color: Colors.white,
-                                              child: ExtendedImage.memory(
-                                                page.bytes,
-                                                fit: BoxFit.fitHeight,
-                                              ),
-                                            ),
-                                          ],
-                                        ))
-                                    .toList(),
-                              );
-                  }
-                  return null; // unreachable
-                }),
+            child: kIsWeb
+                ? FutureBuilder(
+                    future: renderWebPDF(context, invoice),
+                    builder:
+                        (BuildContext context, AsyncSnapshot<String> snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.none:
+                        case ConnectionState.active:
+                        case ConnectionState.waiting:
+                          return LoadingIndicator();
+                        case ConnectionState.done:
+                          if (snapshot.hasError)
+                            return Text(
+                                '${getPdfRequirements(context)} - Error: ${snapshot.error}');
+                          else
+                            return Text('done');
+                      }
+                      return null;
+                    })
+                : FutureBuilder(
+                    future: renderMobilePDF(context, invoice),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<PDFPageImage>> snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.none:
+                        case ConnectionState.active:
+                        case ConnectionState.waiting:
+                          return LoadingIndicator();
+                        case ConnectionState.done:
+                          if (snapshot.hasError)
+                            return Text(
+                                '${getPdfRequirements(context)} - Error: ${snapshot.error}');
+                          else
+                            return snapshot.data.length == 1
+                                ? Center(
+                                    child: Container(
+                                      color: Colors.white,
+                                      child: Image(
+                                          image: MemoryImage(
+                                              snapshot.data.first.bytes),
+                                          height: double.infinity),
+                                    ),
+                                  )
+                                : ListView(
+                                    scrollDirection: Axis.horizontal,
+                                    children: snapshot.data
+                                        .map((page) => Row(
+                                              children: <Widget>[
+                                                Container(
+                                                  width: 20,
+                                                  height: double.infinity,
+                                                  color: Colors.grey,
+                                                ),
+                                                Container(
+                                                  color: Colors.white,
+                                                  child: ExtendedImage.memory(
+                                                    page.bytes,
+                                                    fit: BoxFit.fitHeight,
+                                                  ),
+                                                ),
+                                              ],
+                                            ))
+                                        .toList(),
+                                  );
+                      }
+                      return null; // unreachable
+                    }),
           ),
         );
       });
 }
 
-Future<List<PDFPageImage>> renderPDF(
+Future<HttpClientResponse> _loadPDF(
     BuildContext context, InvoiceEntity invoice) async {
   final invitation = invoice.invitations.first;
   final url = invitation.downloadLink;
@@ -111,9 +140,22 @@ Future<List<PDFPageImage>> renderPDF(
     showErrorDialog(
         context: context,
         message: '${response.statusCode}: ${response.reasonPhrase}');
-    return [];
+    return null;
   }
 
+  return response;
+}
+
+Future<String> renderWebPDF(BuildContext context, InvoiceEntity invoice) async {
+  final response = await _loadPDF(context, invoice);
+  print('Response: $response');
+
+  return 'done';
+}
+
+Future<List<PDFPageImage>> renderMobilePDF(
+    BuildContext context, InvoiceEntity invoice) async {
+  final response = await _loadPDF(context, invoice);
   final bytes = await consolidateHttpClientResponseBytes(response);
   final document = await PDFDocument.openData(bytes);
   final List<PDFPageImage> pages = [];
