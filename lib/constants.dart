25bcc9808b05ed96b8f65aff9f83728a52eaef08
Hillel Coren <hillelcoren@gmail.com>
2020-03-29 21:54:03 +0300

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,7 +7,7 @@ class Constants {
 }
 
 // TODO remove version once #46609 is fixed
-const String kAppVersion = '2.0.4';
+const String kAppVersion = '2.0.5';
 
 const String kSiteUrl = 'https://invoiceninja.com';
 const String kAppProductionUrl = 'https://invoicing.co';
