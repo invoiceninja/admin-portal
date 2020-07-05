cc17d52dd947894ac6069e138bff1c1cba2212e4
Hillel Coren <hillelcoren@gmail.com>
2020-07-05 16:53:55 +0300

    Webhooks

--- lib/ui/webhook/edit/webhook_edit.dart
+++ lib/ui/webhook/edit/webhook_edit.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:invoiceninja_flutter/ui/app/edit_scaffold.dart';
 import 'package:invoiceninja_flutter/ui/app/form_card.dart';
+import 'package:invoiceninja_flutter/ui/app/forms/decorated_form_field.dart';
 import 'package:invoiceninja_flutter/ui/webhook/edit/webhook_edit_vm.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
 import 'package:invoiceninja_flutter/utils/completers.dart';
@@ -19,24 +20,25 @@ class WebhookEdit extends StatefulWidget {
 }
 
 class _WebhookEditState extends State<WebhookEdit> {
+
+  final _urlController = TextEditingController();
+
   static final GlobalKey<FormState> _formKey =
       GlobalKey<FormState>(debugLabel: '_webhookEdit');
   final _debouncer = Debouncer();
-
-  // STARTER: controllers - do not remove comment
 
   List<TextEditingController> _controllers = [];
 
   @override
   void didChangeDependencies() {
     _controllers = [
-      // STARTER: array - do not remove comment
+      _urlController,
     ];
 
     _controllers.forEach((controller) => controller.removeListener(_onChanged));
 
-    final webhook = widget.viewModel.webhook;
-    // STARTER: read value - do not remove comment
+    //final webhook = widget.viewModel.webhook;
+    //_urlController.text = webhook.
 
     _controllers.forEach((controller) => controller.addListener(_onChanged));
 
@@ -95,7 +97,10 @@ class _WebhookEditState extends State<WebhookEdit> {
               children: <Widget>[
                 FormCard(
                   children: <Widget>[
-                    // STARTER: widgets - do not remove comment
+                    DecoratedFormField(
+                      controller: _urlController,
+                      label: localization.url,
+                    )
                   ],
                 ),
               ],
