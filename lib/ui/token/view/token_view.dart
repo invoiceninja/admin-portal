c917cc79785e98afeba74dce68b46bcdc5c57787
Hillel Coren <hillelcoren@gmail.com>
2020-07-08 10:04:30 +0300

    Tokens and wehooks

--- lib/ui/token/view/token_view.dart
+++ lib/ui/token/view/token_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:invoiceninja_flutter/data/models/entities.dart';
+import 'package:invoiceninja_flutter/ui/app/lists/list_divider.dart';
 import 'package:invoiceninja_flutter/ui/app/view_scaffold.dart';
 import 'package:invoiceninja_flutter/ui/token/view/token_view_vm.dart';
+import 'package:invoiceninja_flutter/utils/icons.dart';
+import 'package:invoiceninja_flutter/utils/localization.dart';
 
 class TokenView extends StatefulWidget {
   const TokenView({
@@ -20,6 +25,7 @@ class TokenView extends StatefulWidget {
 class _TokenViewState extends State<TokenView> {
   @override
   Widget build(BuildContext context) {
+    final localization = AppLocalization.of(context);
     final viewModel = widget.viewModel;
     //final userCompany = viewModel.state.userCompany;
     final token = viewModel.token;
@@ -29,7 +35,21 @@ class _TokenViewState extends State<TokenView> {
       onBackPressed: () => viewModel.onBackPressed(),
       entity: token,
       body: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+          SizedBox(height: 16),
+          ListDivider(),
+          ListTile(
+            contentPadding: const EdgeInsets.all(16),
+            title: Text(
+              token.token,
+            ),
+            trailing: Icon(FontAwesomeIcons.copy),
+            onTap: () {
+
+            },
+          ),
+          ListDivider(),
+        ],
       ),
     );
   }
