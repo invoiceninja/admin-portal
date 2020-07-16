f78d1b812d2e5f48f0a33fe06827761aeb72e7cc
Hillel Coren <hillelcoren@gmail.com>
2020-07-16 20:06:12 +0300

    Bug: filter close is broken when always show is enabled

--- lib/ui/app/view_scaffold.dart
+++ lib/ui/app/view_scaffold.dart
@@ -51,8 +51,16 @@ class ViewScaffold extends StatelessWidget {
           entity.entityType == state.uiState.filterEntityType) {
         leading = IconButton(
           icon: Icon(Icons.clear),
-          onPressed: () =>
-              store.dispatch(UserPreferencesChanged(showFilterSidebar: false)),
+          onPressed: () {
+            if (state.prefState.alwaysShowFilterSidebar) {
+              store.dispatch(FilterByEntity(
+                entityType: state.uiState.filterEntityType,
+                entityId: state.uiState.filterEntityId,
+              ));
+            } else {
+              store.dispatch(UserPreferencesChanged(showFilterSidebar: false));
+            }
+          },
         );
       } else if (isSettings) {
         leading = IconButton(
