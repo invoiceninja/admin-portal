a1a59b0f543131f5efd5c6cf7f717c6f3f8a91d5
Hillel Coren <hillelcoren@gmail.com>
2020-09-21 17:10:54 +0300

    Recurring invoices

---
+++ lib/data/models/recurring_invoice_model.dart
@@ -0,0 +1,34 @@
+class RecurringInvoiceFields {
+  static const String amount = 'invoice_amount';
+  static const String clientId = 'client_id';
+  static const String client = 'client';
+  static const String statusId = 'status_id';
+  static const String status = 'status';
+  static const String invoiceNumber = 'invoice_number';
+  static const String discount = 'discount';
+  static const String poNumber = 'po_number';
+  static const String date = 'invoice_date';
+  static const String dueDateDays = 'due_date_days';
+  static const String terms = 'terms';
+  static const String footer = 'footer';
+  static const String publicNotes = 'public_notes';
+  static const String privateNotes = 'private_notes';
+  static const String frequencyId = 'frequency_id';
+  static const String endDate = 'end_date';
+  static const String documents = 'documents';
+  static const String customValue1 = 'custom1';
+  static const String customValue2 = 'custom2';
+  static const String customValue3 = 'custom3';
+  static const String customValue4 = 'custom4';
+  static const String taxAmount = 'tax_amount';
+  static const String reminder1Sent = 'reminder1_sent';
+  static const String reminder2Sent = 'reminder2_sent';
+  static const String reminder3Sent = 'reminder3_sent';
+  static const String reminderLastSent = 'reminder_last_sent';
+  static const String exchangeRate = 'exchange_rate';
+
+  static const String remainingCycles = 'remaining_cycles';
+  static const String frequency = 'frequency';
+  static const String nextSendDate = 'next_send_date';
+  static const String autoBill = 'auto_bill';
+}
