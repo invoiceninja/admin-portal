bcd3469f132e583760cf445c81a14b2b0a3d7e4a
Hillel Coren <hillelcoren@gmail.com>
2020-09-25 17:57:51 +0300

    Recurring invoices

--- lib/ui/invoice/view/invoice_view_schedule.dart
+++ lib/ui/invoice/view/invoice_view_schedule.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceninja_flutter/ui/invoice/view/invoice_view_vm.dart';
+import 'package:invoiceninja_flutter/utils/formatting.dart';
 
 class InvoiceViewSchedule extends StatelessWidget {
   const InvoiceViewSchedule({Key key, @required this.viewModel})
@@ -10,10 +11,22 @@ class InvoiceViewSchedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final invoice = viewModel.invoice;
-
+    print('## recurring dates: ${invoice.recurringDates}');
     return ListView(
       shrinkWrap: true,
       padding: const EdgeInsets.all(8),
+      children: invoice.recurringDates
+          .map((schedule) => Row(
+                children: [
+                  Expanded(
+                    child: Text(formatDate(schedule.sendDate, context)),
+                  ),
+                  Expanded(
+                    child: Text(formatDate(schedule.dueDate, context)),
+                  ),
+                ],
+              ))
+          .toList(),
     );
   }
 }
