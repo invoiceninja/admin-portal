f4549bbd21ac70ffc72b4c4598936a564c26cc0b
Hillel Coren <hillelcoren@gmail.com>
2020-09-25 18:01:18 +0300

    Recurring invoices

--- lib/ui/invoice/view/invoice_view_schedule.dart
+++ lib/ui/invoice/view/invoice_view_schedule.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceninja_flutter/ui/invoice/view/invoice_view_vm.dart';
 import 'package:invoiceninja_flutter/utils/formatting.dart';
+import 'package:invoiceninja_flutter/utils/localization.dart';
 
 class InvoiceViewSchedule extends StatelessWidget {
   const InvoiceViewSchedule({Key key, @required this.viewModel})
@@ -11,22 +12,38 @@ class InvoiceViewSchedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final invoice = viewModel.invoice;
+    final localization = AppLocalization.of(context);
     print('## recurring dates: ${invoice.recurringDates}');
     return ListView(
       shrinkWrap: true,
-      padding: const EdgeInsets.all(8),
-      children: invoice.recurringDates
-          .map((schedule) => Row(
-                children: [
-                  Expanded(
-                    child: Text(formatDate(schedule.sendDate, context)),
+      padding: const EdgeInsets.all(16),
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Text(localization.sendDate),
+            ),
+            Expanded(
+              child: Text(localization.dueDate),
+            ),
+          ],
+        ),
+        ...invoice.recurringDates
+            .map((schedule) => Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(formatDate(schedule.sendDate, context)),
+                      ),
+                      Expanded(
+                        child: Text(formatDate(schedule.dueDate, context)),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: Text(formatDate(schedule.dueDate, context)),
-                  ),
-                ],
-              ))
-          .toList(),
+                ))
+            .toList(),
+      ],
     );
   }
 }
