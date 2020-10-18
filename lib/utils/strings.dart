0ccdf962809b0526c2b0ea03a08ad43535c80c01
Hillel Coren <hillelcoren@gmail.com>
2020-10-18 09:17:02 +0300

    Expenses

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -83,7 +83,7 @@ bool isValidDate(String input) {
 
 void printWrapped(String text) {
   if (text.length > 2000) {
-    text = text.substring(0, 2000);
+    //text = text.substring(0, 2000);
   }
 
   final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
