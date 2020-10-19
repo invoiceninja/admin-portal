e2b158998369b11f8f570f9c98772133bcc03ace
Hillel Coren <hillelcoren@gmail.com>
2020-10-19 10:34:54 +0300

    Tasks

--- lib/ui/task/edit/task_edit.dart
+++ lib/ui/task/edit/task_edit.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:invoiceninja_flutter/constants.dart';
+import 'package:invoiceninja_flutter/ui/app/app_border.dart';
 import 'package:invoiceninja_flutter/ui/app/edit_scaffold.dart';
+import 'package:invoiceninja_flutter/ui/app/live_text.dart';
 import 'package:invoiceninja_flutter/ui/task/edit/task_edit_details_vm.dart';
 import 'package:invoiceninja_flutter/ui/task/edit/task_edit_times_vm.dart';
 import 'package:invoiceninja_flutter/ui/task/edit/task_edit_vm.dart';
+import 'package:invoiceninja_flutter/utils/formatting.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
 
 class TaskEdit extends StatefulWidget {
@@ -106,6 +110,34 @@ class _TaskEditState extends State<TaskEdit>
             TaskEditDetailsScreen(),
             TaskEditTimesScreen(),
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).cardColor,
+        shape: CircularNotchedRectangle(),
+        child: SizedBox(
+          height: kTopBottomBarHeight,
+          child: AppBorder(
+            isTop: true,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: LiveText(() {
+                  return localization.duration +
+                      ' ' +
+                      formatNumber(task.listDisplayAmount, context,
+                          formatNumberType: FormatNumberType.duration);
+                },
+                    style: TextStyle(
+                      color: viewModel.state.prefState.enableDarkMode
+                          ? Colors.white
+                          : Colors.black,
+                      fontSize: 20.0,
+                    )),
+              ),
+            ),
+          ),
         ),
       ),
       floatingActionButton: task.isInvoiced || task.isDeleted
