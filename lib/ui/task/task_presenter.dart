5408f17445da1c3c49277e2c536fba767dac1af7
Gianfranco Gasbarri <gifrgas@gmail.com>
2020-10-22 23:16:14 +0100

    Added Task presenter fields

--- lib/ui/task/task_presenter.dart
+++ lib/ui/task/task_presenter.dart
@@ -4,6 +4,7 @@ import 'package:invoiceninja_flutter/data/models/models.dart';
 import 'package:invoiceninja_flutter/data/models/task_model.dart';
 import 'package:invoiceninja_flutter/redux/app/app_state.dart';
 import 'package:invoiceninja_flutter/ui/app/presenters/entity_presenter.dart';
+import 'package:invoiceninja_flutter/utils/formatting.dart';
 
 class TaskPresenter extends EntityPresenter {
   static List<String> getDefaultTableFields(UserCompanyEntity userCompany) {
@@ -20,13 +21,28 @@ class TaskPresenter extends EntityPresenter {
     return [
       ...getDefaultTableFields(userCompany),
       ...EntityPresenter.getBaseFields(),
+      TaskFields.number,
+      TaskFields.invoiceId,
+      TaskFields.clientId,
+      TaskFields.projectId,
+      TaskFields.timeLog,
+      TaskFields.isRunning,
+      TaskFields.customValue1,
+      TaskFields.customValue2,
+      TaskFields.customValue3,
+      TaskFields.customValue4,
+      TaskFields.documents,
+      TaskFields.updatedAt,
+      TaskFields.archivedAt,
+      TaskFields.isDeleted,
     ];
   }
 
   @override
   Widget getField({String field, BuildContext context}) {
-    final state = StoreProvider.of<AppState>(context).state;
     final task = entity as TaskEntity;
+    final store = StoreProvider.of<AppState>(context);
+    final state = store.state;
 
     switch (field) {
       case TaskFields.client:
@@ -37,6 +53,38 @@ class TaskPresenter extends EntityPresenter {
         return Text(task.description);
       case TaskFields.duration:
         return Text('');
+      case TaskFields.number:
+        return Text(task.number.toString());
+      case TaskFields.invoiceId:
+        return Text(
+            state.invoiceState.map[task.invoiceId]?.listDisplayName ?? '');
+      case TaskFields.clientId:
+        return Text(state.clientState.map[task.clientId]?.displayName ?? '');
+      case TaskFields.projectId:
+        return Text(
+            state.projectState.map[task.projectId]?.listDisplayName ?? '');
+      case TaskFields.timeLog:
+        return Text(task.timeLog);
+      case TaskFields.isRunning:
+        return Text(task.isRunning.toString());
+      case TaskFields.customValue1:
+        return Text(task.customValue1);
+      case TaskFields.customValue2:
+        return Text(task.customValue2);
+      case TaskFields.customValue3:
+        return Text(task.customValue3);
+      case TaskFields.customValue4:
+        return Text(task.customValue4);
+      case TaskFields.documents:
+        return Text('${task.documents.length}');
+      case TaskFields.updatedAt:
+        return Text(
+            formatDate(convertTimestampToDateString(task.updatedAt), context));
+      case TaskFields.archivedAt:
+        return Text(
+            formatDate(convertTimestampToDateString(task.archivedAt), context));
+      case TaskFields.isDeleted:
+        return Text(task.isDeleted.toString());
     }
 
     return super.getField(field: field, context: context);
