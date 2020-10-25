bf33623b34c6a23de2673fe9dba7901b28d6655f
Hillel Coren <hillelcoren@gmail.com>
2020-10-25 15:53:35 +0200

    UI styling

--- lib/ui/company_gateway/edit/company_gateway_edit.dart
+++ lib/ui/company_gateway/edit/company_gateway_edit.dart
@@ -201,7 +201,7 @@ class _CompanyGatewayEditState extends State<CompanyGatewayEdit>
                           const EdgeInsets.only(left: 16, top: 16, bottom: 16),
                       child: Text(
                         localization.acceptedCardLogos,
-                        style: Theme.of(context).textTheme.headline5,
+                        style: Theme.of(context).textTheme.headline6,
                       ),
                     ),
                     CardListTile(
