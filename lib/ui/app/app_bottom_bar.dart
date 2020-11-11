d156711f982a725403aafb868e7d77d9a68b1f50
Hillel Coren <hillelcoren@gmail.com>
2020-11-11 20:08:26 +0200

    Add missing tooltips

--- lib/ui/app/app_bottom_bar.dart
+++ lib/ui/app/app_bottom_bar.dart
@@ -426,6 +426,7 @@ class _AppBottomBarState extends State<AppBottomBar> {
                 ),
                 if (widget.statuses.isNotEmpty)
                   IconButton(
+                    tooltip: localization.filter,
                     icon: Icon(Icons.filter),
                     onPressed: _showFilterStatusSheet,
                     color: store.state
@@ -436,6 +437,7 @@ class _AppBottomBarState extends State<AppBottomBar> {
                   ),
                 if (widget.customValues1.isNotEmpty)
                   IconButton(
+                    tooltip: localization.filter,
                     icon: Icon(Icons.looks_one),
                     onPressed: _showFilterCustom1Sheet,
                     color: store.state
@@ -446,6 +448,7 @@ class _AppBottomBarState extends State<AppBottomBar> {
                   ),
                 if (widget.customValues2.isNotEmpty)
                   IconButton(
+                    tooltip: localization.filter,
                     icon: Icon(Icons.looks_two),
                     onPressed: _showFilterCustom2Sheet,
                     color: store.state
@@ -456,7 +459,8 @@ class _AppBottomBarState extends State<AppBottomBar> {
                   ),
                 if (widget.customValues3.isNotEmpty)
                   IconButton(
-                    icon: Icon(Icons.looks_two),
+                    tooltip: localization.filter,
+                    icon: Icon(Icons.looks_3),
                     onPressed: _showFilterCustom3Sheet,
                     color: store.state
                             .getListState(widget.entityType)
@@ -466,7 +470,8 @@ class _AppBottomBarState extends State<AppBottomBar> {
                   ),
                 if (widget.customValues4.isNotEmpty)
                   IconButton(
-                    icon: Icon(Icons.looks_two),
+                    tooltip: localization.filter,
+                    icon: Icon(Icons.looks_4),
                     onPressed: _showFilterCustom4Sheet,
                     color: store.state
                             .getListState(widget.entityType)
