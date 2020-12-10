8a108ab4f4c9488f25f2543cb5f290984c7d358e
Hillel Coren <hillelcoren@gmail.com>
2020-12-10 21:05:56 +0200

    UI styling

--- lib/ui/dashboard/dashboard_chart.dart
+++ lib/ui/dashboard/dashboard_chart.dart
@@ -4,6 +4,8 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:invoiceninja_flutter/redux/app/app_state.dart';
 import 'package:invoiceninja_flutter/redux/dashboard/dashboard_selectors.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:invoiceninja_flutter/ui/app/app_border.dart';
+import 'package:invoiceninja_flutter/ui/app/form_card.dart';
 import 'package:invoiceninja_flutter/utils/formatting.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
 
@@ -105,131 +107,130 @@ class _DashboardChartState extends State<DashboardChart> {
       ),
     );
 
-    return Container(
-      child: _FormCard(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: Text(
-              widget.title,
-              style: theme.textTheme.headline5,
-            ),
+    return FormCard(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            widget.title,
+            style: theme.textTheme.headline5,
           ),
-          Divider(height: 1.0),
-          LimitedBox(
-            //maxHeight: settings.enableComparison ? 116 : 96,
-            maxHeight: settings.enableComparison ? 120 : 100,
-            child: ListView(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              children: widget.data.map((dataGroup) {
-                final int index = widget.data.indexOf(dataGroup);
-                final bool isSelected = index == _selectedIndex;
-                final bool isIncrease =
-                    dataGroup.total > dataGroup.previousTotal;
-                final String changeAmount = (isIncrease ? '+' : '') +
-                    formatNumber(
-                        dataGroup.total - dataGroup.previousTotal, context,
-                        currencyId: widget.currencyId);
-                final changePercent = (isIncrease ? '+' : '') +
-                    formatNumber(
-                        dataGroup.total != 0 && dataGroup.previousTotal != 0
-                            ? round(
-                                (dataGroup.total - dataGroup.previousTotal) /
-                                    dataGroup.previousTotal *
-                                    100,
-                                2)
-                            : 0.0,
-                        context,
-                        formatNumberType: FormatNumberType.percent,
-                        currencyId: widget.currencyId);
-                final String changeString = dataGroup.total == 0 ||
-                        dataGroup.previousTotal == 0 ||
-                        dataGroup.total == dataGroup.previousTotal
-                    ? (settings.enableComparison ? ' ' : '')
-                    : '$changeAmount ($changePercent)';
+        ),
+        Divider(height: 1.0),
+        LimitedBox(
+          //maxHeight: settings.enableComparison ? 116 : 96,
+          maxHeight: settings.enableComparison ? 120 : 100,
+          child: ListView(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            children: widget.data.map((dataGroup) {
+              final int index = widget.data.indexOf(dataGroup);
+              final bool isSelected = index == _selectedIndex;
+              final bool isIncrease =
+                  dataGroup.total > dataGroup.previousTotal;
+              final String changeAmount = (isIncrease ? '+' : '') +
+                  formatNumber(
+                      dataGroup.total - dataGroup.previousTotal, context,
+                      currencyId: widget.currencyId);
+              final changePercent = (isIncrease ? '+' : '') +
+                  formatNumber(
+                      dataGroup.total != 0 && dataGroup.previousTotal != 0
+                          ? round(
+                              (dataGroup.total - dataGroup.previousTotal) /
+                                  dataGroup.previousTotal *
+                                  100,
+                              2)
+                          : 0.0,
+                      context,
+                      formatNumberType: FormatNumberType.percent,
+                      currencyId: widget.currencyId);
+              final String changeString = dataGroup.total == 0 ||
+                      dataGroup.previousTotal == 0 ||
+                      dataGroup.total == dataGroup.previousTotal
+                  ? (settings.enableComparison ? ' ' : '')
+                  : '$changeAmount ($changePercent)';
 
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      _selectedIndex = index;
-                      _selected = null;
-                    });
-                  },
-                  child: Container(
-                    color: isSelected ? state.accentColor : theme.cardColor,
-                    padding: EdgeInsets.only(
-                        left: 16, top: 16, right: 32, bottom: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(localization.lookup(dataGroup.name),
-                            style: theme.textTheme.headline6.copyWith(
-                              color: isSelected ? Colors.white : null,
-                            )),
-                        SizedBox(height: 4),
-                        Text(
-                            formatNumber(dataGroup.total, context,
-                                currencyId: widget.currencyId),
-                            style: theme.textTheme.headline5.copyWith(
-                                color: isSelected ? Colors.white : null)),
-                        SizedBox(height: 4),
-                        changeString.isNotEmpty
-                            ? Text(
-                                changeString,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: isSelected
-                                      ? Colors.white
-                                      : (isIncrease
-                                          ? Colors.green
-                                          : Colors.red),
-                                ),
-                              )
-                            : SizedBox(),
-                      ],
-                    ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-          Divider(height: 1.0),
-          SizedBox(
-            height: 240.0,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ClipRect(
-                child: chart,
-              ),
-            ),
-          ),
-          Divider(height: 1.0),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    localization.average +
-                        ': ' +
-                        formatNumber(series.average, context,
-                            currencyId: widget.currencyId),
-                    style: theme.textTheme.headline5,
+              return InkWell(
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = index;
+                    _selected = null;
+                  });
+                },
+                child: Container(
+                  color: isSelected ? state.accentColor : theme.cardColor,
+                  padding: EdgeInsets.only(
+                      left: 16, top: 16, right: 32, bottom: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(localization.lookup(dataGroup.name),
+                          style: theme.textTheme.headline6.copyWith(
+                            color: isSelected ? Colors.white : null,
+                          )),
+                      SizedBox(height: 4),
+                      Text(
+                          formatNumber(dataGroup.total, context,
+                              currencyId: widget.currencyId),
+                          style: theme.textTheme.headline5.copyWith(
+                              color: isSelected ? Colors.white : null)),
+                      SizedBox(height: 4),
+                      changeString.isNotEmpty
+                          ? Text(
+                              changeString,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: isSelected
+                                    ? Colors.white
+                                    : (isIncrease
+                                        ? Colors.green
+                                        : Colors.red),
+                              ),
+                            )
+                          : SizedBox(),
+                    ],
                   ),
                 ),
-                _selected != null
-                    ? Text(
-                        _selected,
-                        style: theme.textTheme.headline5,
-                      )
-                    : SizedBox(),
-              ],
+              );
+            }).toList(),
+          ),
+        ),
+        Divider(height: 1.0),
+        SizedBox(
+          height: 240.0,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ClipRect(
+              child: chart,
             ),
           ),
-        ],
-      ),
+        ),
+        Divider(height: 1.0),
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  localization.average +
+                      ': ' +
+                      formatNumber(series.average, context,
+                          currencyId: widget.currencyId),
+                  style: theme.textTheme.headline5,
+                ),
+              ),
+              _selected != null
+                  ? Text(
+                      _selected,
+                      style: theme.textTheme.headline5,
+                    )
+                  : SizedBox(),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
