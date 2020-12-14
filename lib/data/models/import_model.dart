b464be5e4fd7c8243d3531b9f06187c45d298261
Hillel Coren <hillelcoren@gmail.com>
2020-12-14 09:00:21 +0200

    CSV import

---
+++ lib/data/models/import_model.dart
@@ -0,0 +1,25 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+
+part 'import_model.g.dart';
+
+abstract class PreImportResponse
+    implements Built<PreImportResponse, PreImportResponseBuilder> {
+  factory PreImportResponse() {
+    return _$PreImportResponse._();
+  }
+
+  PreImportResponse._();
+
+  @override
+  @memoized
+  int get hashCode;
+
+  String get hash;
+
+  BuiltList<BuiltList<String>> get headers
+
+  static Serializer<PreImportResponse> get serializer =>
+      _$preImportResponseSerializer;
+}
