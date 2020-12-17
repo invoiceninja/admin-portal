bd6879201eee4c84053ea7850e92e9c5e85fcd56
Hillel Coren <hillelcoren@gmail.com>
2020-12-17 13:05:16 +0200

    Models

--- lib/data/models/company_model.g.dart
+++ lib/data/models/company_model.g.dart
@@ -913,9 +913,6 @@ class _$UserSettingsEntitySerializer
   Iterable<Object> serialize(Serializers serializers, UserSettingsEntity object,
       {FullType specifiedType = FullType.unspecified}) {
     final result = <Object>[
-      'accent_color',
-      serializers.serialize(object.accentColor,
-          specifiedType: const FullType(String)),
       'table_columns',
       serializers.serialize(object.tableColumns,
           specifiedType: const FullType(BuiltMap, const [
@@ -929,7 +926,12 @@ class _$UserSettingsEntitySerializer
             const FullType(ReportSettingsEntity)
           ])),
     ];
-
+    if (object.accentColor != null) {
+      result
+        ..add('accent_color')
+        ..add(serializers.serialize(object.accentColor,
+            specifiedType: const FullType(String)));
+    }
     return result;
   }
 
@@ -4629,9 +4631,6 @@ class _$UserSettingsEntity extends UserSettingsEntity {
   _$UserSettingsEntity._(
       {this.accentColor, this.tableColumns, this.reportSettings})
       : super._() {
-    if (accentColor == null) {
-      throw new BuiltValueNullFieldError('UserSettingsEntity', 'accentColor');
-    }
     if (tableColumns == null) {
       throw new BuiltValueNullFieldError('UserSettingsEntity', 'tableColumns');
     }
