1e9fd927295682a1cba8db81b66dc9b13d14cb26
Hillel Coren <hillelcoren@gmail.com>
2020-12-22 23:28:11 +0200

    Update version

--- lib/redux/app/app_state.dart
+++ lib/redux/app/app_state.dart
@@ -748,17 +748,6 @@ abstract class AppState implements Built<AppState, AppStateBuilder> {
     //return 'LOG: ${clientState.map[clientUIState.selectedId]?.systemLogs ?? ''}';
     //return 'FREQ: ${recurringInvoiceUIState.editing.frequencyId}';
 
-    return 'Task Client Id: ${taskUIState.editing.clientId}';
-
-    /*
-    final times = taskUIState.editing.getTaskTimes();
-    var str = '\n\nTask:';
-    times.forEach((time) {
-      str += '\nStart: ${time.startDate} ${time.startDate.isUtc} - End: ${time.endDate} ${time.endDate?.isUtc}';
-    });
-    return str + '\n\n';
-     */
-
     return '\n\nURL: ${authState.url}'
         '\nRoute: ${uiState.currentRoute}'
         '\nPrevious: ${uiState.previousRoute}'
