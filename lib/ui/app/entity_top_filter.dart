17acc3fae081d72711c64378a35e87132a99a363
Hillel Coren <hillelcoren@gmail.com>
2021-02-21 08:52:19 +0200

    Code refactor

---
+++ lib/ui/app/entity_top_filter.dart
@@ -0,0 +1,207 @@
+import 'package:invoiceninja_flutter/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:invoiceninja_flutter/data/models/entities.dart';
+import 'package:invoiceninja_flutter/data/models/models.dart';
+import 'package:invoiceninja_flutter/redux/app/app_actions.dart';
+import 'package:invoiceninja_flutter/redux/app/app_state.dart';
+import 'package:invoiceninja_flutter/ui/app/presenters/entity_presenter.dart';
+import 'package:invoiceninja_flutter/utils/localization.dart';
+import 'package:overflow_view/overflow_view.dart';
+
+class EntityTopFilter extends StatelessWidget {
+  const EntityTopFilter({@required this.show});
+
+  final bool show;
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalization.of(context);
+    final store = StoreProvider.of<AppState>(context);
+    final state = store.state;
+    final uiState = state.uiState;
+
+    final filterEntityType = uiState.filterEntityType;
+    final routeEntityType = uiState.entityTypeRoute;
+
+    final entityMap =
+        filterEntityType != null ? state.getEntityMap(filterEntityType) : null;
+    final filterEntity =
+        entityMap != null ? entityMap[uiState.filterEntityId] : null;
+    final relatedTypes = filterEntityType?.relatedTypes
+            ?.where((element) => state.company.isModuleEnabled(element))
+            ?.toList() ??
+        [];
+
+    final backgroundColor =
+        !state.prefState.enableDarkMode && state.hasAccentColor
+            ? state.accentColor
+            : Theme.of(context).cardColor;
+
+    return Material(
+      color: backgroundColor,
+      child: AnimatedContainer(
+        height: show ? 46 : 0,
+        duration: Duration(milliseconds: kDefaultAnimationDuration),
+        curve: Curves.easeInOutCubic,
+        child: AnimatedOpacity(
+          opacity: show ? 1 : 0,
+          duration: Duration(milliseconds: kDefaultAnimationDuration),
+          curve: Curves.easeInOutCubic,
+          child: filterEntity == null
+              ? Container(
+                  color: backgroundColor,
+                )
+              : Row(
+                  children: [
+                    SizedBox(width: 8),
+                    if (!state.prefState.showFilterSidebar)
+                      IconButton(
+                        tooltip: localization.showSidebar,
+                        icon: Icon(
+                          Icons.chrome_reader_mode,
+                          color: state.headerTextColor,
+                        ),
+                        onPressed: () => store.dispatch(
+                            UpdateUserPreferences(showFilterSidebar: true)),
+                      ),
+                    SizedBox(width: 4),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: 220),
+                      child: TextButton(
+                        child: Text(
+                          EntityPresenter()
+                              .initialize(filterEntity, context)
+                              .title,
+                          style: TextStyle(
+                              fontSize: 17, color: state.headerTextColor),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                        onPressed: () => viewEntity(
+                          entity: filterEntity,
+                          context: context,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: OverflowView.flexible(
+                          spacing: 4,
+                          children: <Widget>[
+                            for (int i = 0; i < relatedTypes.length; i++)
+                              DecoratedBox(
+                                child: TextButton(
+                                  child: Text(
+                                    localization
+                                        .lookup('${relatedTypes[i].plural}'),
+                                    style: TextStyle(
+                                      color: state.headerTextColor,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    viewEntitiesByType(
+                                      context: context,
+                                      entityType: relatedTypes[i],
+                                      filterEntity: filterEntity,
+                                    );
+                                  },
+                                  onLongPress: () {
+                                    handleEntityAction(
+                                        context,
+                                        filterEntity,
+                                        EntityAction.newEntityType(
+                                            relatedTypes[i]));
+                                  },
+                                ),
+                                decoration: BoxDecoration(
+                                  border: relatedTypes[i] == routeEntityType
+                                      ? Border(
+                                          bottom: BorderSide(
+                                            color: state.prefState
+                                                        .enableDarkMode ||
+                                                    !state.hasAccentColor
+                                                ? state.accentColor
+                                                : Colors.white,
+                                            width: 2,
+                                          ),
+                                        )
+                                      : null,
+                                ),
+                              )
+                          ],
+                          builder: (context, remaining) {
+                            return PopupMenuButton<EntityType>(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      localization.more,
+                                      style: TextStyle(
+                                          color: state.headerTextColor),
+                                    ),
+                                    SizedBox(width: 4),
+                                    Icon(Icons.arrow_drop_down,
+                                        color: state.headerTextColor),
+                                  ],
+                                ),
+                              ),
+                              initialValue: routeEntityType,
+                              onSelected: (EntityType value) {
+                                if (value == filterEntityType) {
+                                  viewEntity(
+                                    entity: filterEntity,
+                                    context: context,
+                                  );
+                                } else {
+                                  viewEntitiesByType(
+                                    context: context,
+                                    entityType: value,
+                                    filterEntity: filterEntity,
+                                  );
+                                }
+                              },
+                              itemBuilder: (BuildContext context) =>
+                                  filterEntityType.relatedTypes
+                                      .sublist(relatedTypes.length - remaining)
+                                      .where((element) => state.company
+                                          .isModuleEnabled(element))
+                                      .map((type) => PopupMenuItem<EntityType>(
+                                            value: type,
+                                            child: ConstrainedBox(
+                                              constraints: BoxConstraints(
+                                                minWidth: 75,
+                                              ),
+                                              child: Text(type ==
+                                                      filterEntityType
+                                                  ? localization.overview
+                                                  : '${localization.lookup(type.plural)}'),
+                                            ),
+                                          ))
+                                      .toList(),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 4),
+                    IconButton(
+                      icon: Icon(
+                        Icons.clear,
+                        color: state.headerTextColor,
+                      ),
+                      onPressed: () => store.dispatch(FilterByEntity(
+                        entityId: uiState.filterEntityId,
+                        entityType: uiState.filterEntityType,
+                      )),
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/app/main_screen.dart
+++ lib/ui/app/main_screen.dart
@@ -12,10 +12,10 @@ import 'package:invoiceninja_flutter/redux/ui/pref_state.dart';
 import 'package:invoiceninja_flutter/ui/app/app_shortcuts.dart';
 import 'package:invoiceninja_flutter/ui/app/blank_screen.dart';
 import 'package:invoiceninja_flutter/ui/app/change_layout_banner.dart';
+import 'package:invoiceninja_flutter/ui/app/entity_top_filter.dart';
 import 'package:invoiceninja_flutter/ui/app/history_drawer_vm.dart';
 import 'package:invoiceninja_flutter/ui/app/loading_indicator.dart';
 import 'package:invoiceninja_flutter/ui/app/menu_drawer_vm.dart';
-import 'package:invoiceninja_flutter/ui/app/presenters/entity_presenter.dart';
 import 'package:invoiceninja_flutter/ui/app/screen_imports.dart';
 import 'package:invoiceninja_flutter/ui/credit/credit_email_vm.dart';
 import 'package:invoiceninja_flutter/ui/credit/credit_pdf_vm.dart';
@@ -58,7 +58,6 @@ import 'package:invoiceninja_flutter/ui/webhook/view/webhook_view_vm.dart';
 import 'package:invoiceninja_flutter/ui/webhook/webhook_screen_vm.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
 import 'package:invoiceninja_flutter/ui/app/app_border.dart';
-import 'package:overflow_view/overflow_view.dart';
 import 'package:redux/redux.dart';
 
 class MainScreen extends StatelessWidget {
@@ -543,7 +542,7 @@ class EntityScreens extends StatelessWidget {
       }
     }
 
-    topFilterChild = _EntityFilter(
+    topFilterChild = EntityTopFilter(
       show: uiState.filterEntityType != null,
     );
 
@@ -824,202 +823,5 @@ class SettingsScreens extends StatelessWidget {
           isLeft: true,
         ),
     ]);
-  }
-}
-
-class _EntityFilter extends StatelessWidget {
-  const _EntityFilter({@required this.show});
-
-  final bool show;
-
-  @override
-  Widget build(BuildContext context) {
-    final localization = AppLocalization.of(context);
-    final store = StoreProvider.of<AppState>(context);
-    final state = store.state;
-    final uiState = state.uiState;
-
-    final filterEntityType = uiState.filterEntityType;
-    final routeEntityType = uiState.entityTypeRoute;
-
-    final entityMap =
-        filterEntityType != null ? state.getEntityMap(filterEntityType) : null;
-    final filterEntity =
-        entityMap != null ? entityMap[uiState.filterEntityId] : null;
-    final relatedTypes = filterEntityType?.relatedTypes
-            ?.where((element) => state.company.isModuleEnabled(element))
-            ?.toList() ??
-        [];
-
-    final backgroundColor =
-        !state.prefState.enableDarkMode && state.hasAccentColor
-            ? state.accentColor
-            : Theme.of(context).cardColor;
-
-    return Material(
-      color: backgroundColor,
-      child: AnimatedContainer(
-        height: show ? 46 : 0,
-        duration: Duration(milliseconds: kDefaultAnimationDuration),
-        curve: Curves.easeInOutCubic,
-        child: AnimatedOpacity(
-          opacity: show ? 1 : 0,
-          duration: Duration(milliseconds: kDefaultAnimationDuration),
-          curve: Curves.easeInOutCubic,
-          child: filterEntity == null
-              ? Container(
-                  color: backgroundColor,
-                )
-              : Row(
-                  children: [
-                    SizedBox(width: 8),
-                    if (!state.prefState.showFilterSidebar)
-                      IconButton(
-                        tooltip: localization.showSidebar,
-                        icon: Icon(
-                          Icons.chrome_reader_mode,
-                          color: state.headerTextColor,
-                        ),
-                        onPressed: () => store.dispatch(
-                            UpdateUserPreferences(showFilterSidebar: true)),
-                      ),
-                    SizedBox(width: 4),
-                    ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: 220),
-                      child: TextButton(
-                        child: Text(
-                          EntityPresenter()
-                              .initialize(filterEntity, context)
-                              .title,
-                          style: TextStyle(
-                              fontSize: 17, color: state.headerTextColor),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                        onPressed: () => viewEntity(
-                          entity: filterEntity,
-                          context: context,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: OverflowView.flexible(
-                          spacing: 4,
-                          children: <Widget>[
-                            for (int i = 0; i < relatedTypes.length; i++)
-                              DecoratedBox(
-                                child: TextButton(
-                                  child: Text(
-                                    localization
-                                        .lookup('${relatedTypes[i].plural}'),
-                                    style: TextStyle(
-                                      color: state.headerTextColor,
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    viewEntitiesByType(
-                                      context: context,
-                                      entityType: relatedTypes[i],
-                                      filterEntity: filterEntity,
-                                    );
-                                  },
-                                  onLongPress: () {
-                                    handleEntityAction(
-                                        context,
-                                        filterEntity,
-                                        EntityAction.newEntityType(
-                                            relatedTypes[i]));
-                                  },
-                                ),
-                                decoration: BoxDecoration(
-                                  border: relatedTypes[i] == routeEntityType
-                                      ? Border(
-                                          bottom: BorderSide(
-                                            color: state.prefState
-                                                        .enableDarkMode ||
-                                                    !state.hasAccentColor
-                                                ? state.accentColor
-                                                : Colors.white,
-                                            width: 2,
-                                          ),
-                                        )
-                                      : null,
-                                ),
-                              )
-                          ],
-                          builder: (context, remaining) {
-                            return PopupMenuButton<EntityType>(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 8),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      localization.more,
-                                      style: TextStyle(
-                                          color: state.headerTextColor),
-                                    ),
-                                    SizedBox(width: 4),
-                                    Icon(Icons.arrow_drop_down,
-                                        color: state.headerTextColor),
-                                  ],
-                                ),
-                              ),
-                              initialValue: routeEntityType,
-                              onSelected: (EntityType value) {
-                                if (value == filterEntityType) {
-                                  viewEntity(
-                                    entity: filterEntity,
-                                    context: context,
-                                  );
-                                } else {
-                                  viewEntitiesByType(
-                                    context: context,
-                                    entityType: value,
-                                    filterEntity: filterEntity,
-                                  );
-                                }
-                              },
-                              itemBuilder: (BuildContext context) =>
-                                  filterEntityType.relatedTypes
-                                      .sublist(relatedTypes.length - remaining)
-                                      .where((element) => state.company
-                                          .isModuleEnabled(element))
-                                      .map((type) => PopupMenuItem<EntityType>(
-                                            value: type,
-                                            child: ConstrainedBox(
-                                              constraints: BoxConstraints(
-                                                minWidth: 75,
-                                              ),
-                                              child: Text(type ==
-                                                      filterEntityType
-                                                  ? localization.overview
-                                                  : '${localization.lookup(type.plural)}'),
-                                            ),
-                                          ))
-                                      .toList(),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 4),
-                    IconButton(
-                      icon: Icon(
-                        Icons.clear,
-                        color: state.headerTextColor,
-                      ),
-                      onPressed: () => store.dispatch(FilterByEntity(
-                        entityId: uiState.filterEntityId,
-                        entityType: uiState.filterEntityType,
-                      )),
-                    ),
-                  ],
-                ),
-        ),
-      ),
-    );
   }
 }
