704194f73ad5d7ce608217aef670ad2e7ee70627
Hillel Coren <hillelcoren@gmail.com>
2021-03-23 11:01:18 +0200

    Wizard

--- lib/ui/dashboard/dashboard_screen.dart
+++ lib/ui/dashboard/dashboard_screen.dart
@@ -74,7 +74,7 @@ class _DashboardScreenState extends State<DashboardScreen>
         ScrollController(initialScrollOffset: index * kDashboardPanelHeight)
           ..addListener(onScrollListener);
 
-    if (true && (state.company.settings.name ?? '').isEmpty) {
+    if (!kReleaseMode && (state.company.settings.name ?? '').isEmpty) {
       WidgetsBinding.instance.addPostFrameCallback((duration) {
         showDialog<void>(
             context: context,
