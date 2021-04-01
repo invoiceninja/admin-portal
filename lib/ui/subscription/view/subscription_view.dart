359349e29ba62f16f4ad0b698bd4aaaa057b1e4c
Hillel Coren <hillelcoren@gmail.com>
2021-04-01 15:49:48 +0300

    Subscriptions

--- lib/ui/subscription/view/subscription_view.dart
+++ lib/ui/subscription/view/subscription_view.dart
@@ -13,6 +13,7 @@ import 'package:invoiceninja_flutter/ui/subscription/view/subscription_view_vm.d
 import 'package:invoiceninja_flutter/ui/app/view_scaffold.dart';
 import 'package:invoiceninja_flutter/utils/formatting.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SubscriptionView extends StatefulWidget {
   const SubscriptionView({
@@ -60,6 +61,7 @@ class _SubscriptionViewState extends State<SubscriptionView> {
               showToast(localization.copiedToClipboard
                   .replaceFirst(':value', subscription.purchasePage));
             },
+            onLongPress: () => launch(subscription.purchasePage),
           ),
           ListDivider(),
           if (subscription.groupId.isNotEmpty)
