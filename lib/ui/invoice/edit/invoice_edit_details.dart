5be68f6cf8f0951e74b225ac72539f419b9e63d7
Hillel Coren <hillelcoren@gmail.com>
2021-04-21 23:28:48 +0300

    Prevent sidebar editor for allowing dates in the past

--- lib/ui/invoice/edit/invoice_edit_details.dart
+++ lib/ui/invoice/edit/invoice_edit_details.dart
@@ -182,12 +182,14 @@ class InvoiceEditDetailsState extends State<InvoiceEditDetails> {
                           ))
                       .toList()),
               DatePicker(
-                  labelText: (invoice.lastSentDate ?? '').isNotEmpty
-                      ? localization.nextSendDate
-                      : localization.startDate,
-                  onSelected: (date) => viewModel.onChanged(
-                      invoice.rebuild((b) => b..nextSendDate = date)),
-                  selectedDate: invoice.nextSendDate),
+                labelText: (invoice.lastSentDate ?? '').isNotEmpty
+                    ? localization.nextSendDate
+                    : localization.startDate,
+                onSelected: (date) => viewModel
+                    .onChanged(invoice.rebuild((b) => b..nextSendDate = date)),
+                selectedDate: invoice.nextSendDate,
+                firstDate: DateTime.now(),
+              ),
               AppDropdownButton<int>(
                 showUseDefault: true,
                 labelText: localization.remainingCycles,
