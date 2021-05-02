d6cd7405323efc18ff182a1b9ee79d71af583303
Hillel Coren <hillelcoren@gmail.com>
2021-05-02 13:01:02 +0300

    Apply flat button styling globally, ie. dialogs

--- lib/ui/app/buttons/app_text_button.dart
+++ lib/ui/app/buttons/app_text_button.dart
@@ -30,11 +30,6 @@ class AppTextButton extends StatelessWidget {
                   : state.prefState.enableDarkMode
                       ? Colors.white
                       : Colors.black87,
-      minimumSize: Size(88, 36),
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(2)),
-      ),
     );
 
     return TextButton(
