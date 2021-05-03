14b829b065a2dd195dbb84f0ee5666d0b4621e57
Hillel Coren <hillelcoren@gmail.com>
2021-05-03 17:00:20 +0300

    Tests

--- test_driver/all_it_test.dart
+++ test_driver/all_it_test.dart
@@ -1,15 +1,15 @@
-import 'login_it_test.dart' as login;
 import 'clients_it_test.dart' as clients;
 import 'invoices_it_test.dart' as invoices;
+import 'login_it_test.dart' as login;
 import 'products_it_test.dart' as products;
 import 'quotes_it_test.dart' as quotes;
 import 'vendors_it_test.dart' as vendors;
 
 void main() {
-  //login.main();
-  //products.runTestSuite(batchMode: true);
-  //clients.runTestSuite(batchMode: true);
+  login.main();
+  products.runTestSuite(batchMode: true);
+  clients.runTestSuite(batchMode: true);
   invoices.runTestSuite(batchMode: true);
-  //quotes.runTestSuite(batchMode: true);
-  //vendors.runTestSuite(batchMode: true);
+  quotes.runTestSuite(batchMode: true);
+  vendors.runTestSuite(batchMode: true);
 }
