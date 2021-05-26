9e04b4b7906cd55a2b6da87303f781928c208303
Hillel Coren <hillelcoren@gmail.com>
2021-05-26 19:17:50 +0300

    Clean up code warnings

--- lib/ui/app/menu_drawer.dart
+++ lib/ui/app/menu_drawer.dart
@@ -911,7 +911,7 @@ void _showAbout(BuildContext context) async {
                 child: Text(localization.viewLicenses.toUpperCase()),
                 onPressed: () => showLicensePage(
                   context: context,
-                  applicationName: 'Invoice Ninja v5',
+                  applicationName: 'Invoice Ninja',
                   applicationIcon: apppIcon,
                   applicationLegalese: appLegalese,
                   applicationVersion: state.appVersion,
