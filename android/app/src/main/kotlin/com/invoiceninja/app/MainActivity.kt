3ab501911512523ff2c1e9eb3d315cb37d833465
Zane Campbell <development@zappcodestudios.com>
2021-06-12 04:20:37 -0400

    Switched from FlutterActivity to FlutterFragmentActivity

--- android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.invoiceninja.app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
