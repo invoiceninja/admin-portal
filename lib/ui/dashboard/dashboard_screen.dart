fc024497c4f74161af6b83c5960c9d7225f70094
Hillel Coren <hillelcoren@gmail.com>
2021-06-22 10:56:52 +0300

    Explain migration in wizard

--- lib/ui/dashboard/dashboard_screen.dart
+++ lib/ui/dashboard/dashboard_screen.dart
@@ -75,8 +75,7 @@ class _DashboardScreenState extends State<DashboardScreen>
         ScrollController(initialScrollOffset: index * kDashboardPanelHeight)
           ..addListener(onScrollListener);
 
-    if (!kReleaseMode ||
-        !state.isDemo && (state.company.settings.name ?? '').isEmpty) {
+    if (!state.isDemo && (state.company.settings.name ?? '').isEmpty) {
       WidgetsBinding.instance.addPostFrameCallback((duration) {
         showDialog<void>(
             context: context,
