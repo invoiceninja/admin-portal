d49ae6e5933d59663a084b341221cd43f821042a
Hillel Coren <hillelcoren@gmail.com>
2021-06-27 11:18:18 +0300

    Hide invitations for deleted contacts

--- lib/main.dart
+++ lib/main.dart
@@ -161,7 +161,7 @@ Future<AppState> _initialState(bool isTesting) async {
   if (kIsWeb) {
     reportErrors = WebUtils.getHtmlValue('report-errors') == '1';
     if (reportErrors) {
-      print('## ERROR reporting is enabled');
+      print('## Error reporting is enabled');
     }
   }
 
