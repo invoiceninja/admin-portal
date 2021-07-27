cea5b204c2cf01e6a2a4bd40c7843d7720209e50
Hillel Coren <hillelcoren@gmail.com>
2021-07-27 14:44:37 +0300

    Bug: support clearing accent color

--- lib/data/models/company_model.dart
+++ lib/data/models/company_model.dart
@@ -864,7 +864,7 @@ abstract class UserSettingsEntity
 
   // ignore: unused_element
   static void _initializeBuilder(UserSettingsEntityBuilder builder) => builder
-    ..accentColor = kDefaultAccentColor
+    //..accentColor = kDefaultAccentColor
     ..numberYearsActive = 3
     ..tableColumns.replace(BuiltMap<String, BuiltList<String>>())
     ..reportSettings.replace(BuiltMap<String, ReportSettingsEntity>())
