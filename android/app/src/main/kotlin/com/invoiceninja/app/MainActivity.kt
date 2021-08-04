63726e87fc62c5a57fcaf06addecc2c924d52140
Hillel Coren <hillelcoren@gmail.com>
2021-08-04 16:20:56 +0300

    Correct fingerprint feature on Android

--- android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.invoiceninja.app
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
