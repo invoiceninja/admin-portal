e8c4512601190b17ad2adec7c622dcfe7823aeec
Hillel Coren <hillelcoren@gmail.com>
2021-08-10 22:15:01 +0300

    Update Flutter version

--- lib/ui/expense/edit/expense_edit_settings.dart
+++ lib/ui/expense/edit/expense_edit_settings.dart
@@ -6,7 +6,6 @@ import 'package:invoiceninja_flutter/ui/app/entity_dropdown.dart';
 import 'package:invoiceninja_flutter/ui/app/forms/bool_dropdown_button.dart';
 import 'package:invoiceninja_flutter/ui/app/forms/date_picker.dart';
 import 'package:invoiceninja_flutter/ui/app/forms/decorated_form_field.dart';
-import 'package:invoiceninja_flutter/ui/app/help_text.dart';
 import 'package:invoiceninja_flutter/ui/app/scrollable_listview.dart';
 import 'package:invoiceninja_flutter/ui/expense/edit/expense_edit_vm.dart';
 import 'package:invoiceninja_flutter/utils/completers.dart';
