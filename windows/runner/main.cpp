1da1ec0e8eff1e9a8bdd6d69e27aa4c191944e06
Hillel Coren <hillelcoren@gmail.com>
2021-08-23 00:00:42 +0300

    Update windows files

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -30,7 +30,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"invoiceninja_client", origin, size)) {
+  if (!window.CreateAndShow(L"Invoice Ninja", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
