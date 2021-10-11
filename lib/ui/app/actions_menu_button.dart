ef1e66fc80d247305a8e8cb694c68b5fac24df76
Hillel Coren <hillelcoren@gmail.com>
2021-10-11 15:51:22 +0300

    Adjust loading indicator

--- lib/ui/app/actions_menu_button.dart
+++ lib/ui/app/actions_menu_button.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:invoiceninja_flutter/data/models/models.dart';
-import 'package:invoiceninja_flutter/redux/app/app_state.dart';
 import 'package:invoiceninja_flutter/utils/icons.dart';
 import 'package:invoiceninja_flutter/utils/localization.dart';
 
@@ -26,8 +24,6 @@ class ActionMenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final store = StoreProvider.of<AppState>(context);
-    final state = store.state;
     final List<PopupMenuEntry<EntityAction>> actions = [];
 
     if (isSaving) {
