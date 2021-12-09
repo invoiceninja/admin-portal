662394526e46071e9de838b1a659ca9c466d59dc
Hillel Coren <hillelcoren@gmail.com>
2021-12-09 16:34:25 +0200

    Don't show partial invoices as viewed

--- lib/ui/payment/edit/payment_edit.dart
+++ lib/ui/payment/edit/payment_edit.dart
@@ -201,7 +201,7 @@ class _PaymentEditState extends State<PaymentEdit> {
             children: <Widget>[
               if (payment.isNew) ...[
                 EntityDropdown(
-                  autofocus: true,
+                  autofocus: (payment.clientId ?? '').isEmpty,
                   entityType: EntityType.client,
                   labelText: AppLocalization.of(context).client,
                   entityId: payment.clientId,
