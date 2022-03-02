59d5c540be09b881a723bd1d94701461ed6c1d9c
Hillel Coren <hillelcoren@gmail.com>
2022-03-02 21:25:50 +0200

    Don’t show null/null in gateway card details

--- lib/ui/app/gateways/token_meta.dart
+++ lib/ui/app/gateways/token_meta.dart
@@ -11,6 +11,15 @@ class TokenMeta extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var cardDetails = '••••';
+
+    if (meta.last4 != null) {
+      cardDetails += ' ${meta.last4}';
+    }
+    if (meta.expMonth != null && meta.expYear != null) {
+      cardDetails += '  ${meta.expMonth}/${meta.expYear}';
+    }
+
     return Row(
       children: [
         Image.asset(
@@ -20,7 +29,7 @@ class TokenMeta extends StatelessWidget {
         SizedBox(width: 8),
         Flexible(
           child: Text(
-            '•••• ${meta.last4} ${meta.expMonth}/${meta.expYear}',
+            cardDetails,
             overflow: TextOverflow.ellipsis,
           ),
         ),
