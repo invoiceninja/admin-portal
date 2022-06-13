41885a195fdea551e6287cf88eb518fe65804115
Hillel Coren <hillelcoren@gmail.com>
2022-06-13 13:46:30 +0300

    Browser is stuck on loading - Max execution time exceeded #447

--- lib/redux/app/app_state.dart
+++ lib/redux/app/app_state.dart
@@ -753,6 +753,9 @@ abstract class AppState implements Built<AppState, AppStateBuilder> {
 
   bool get reportErrors => account?.reportErrors ?? false;
 
+  int get recordsPerPage =>
+      isHosted ? kHostedRecordsPerPage : kSelfhostedRecordsPerPage;
+
   bool get isHosted => account == null ? authState.isHosted : account.isHosted;
 
   bool get isSelfHosted => !isHosted;
