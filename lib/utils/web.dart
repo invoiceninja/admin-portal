599aa190c19a9b86441ab5b29db50e27c375bc8f
Hillel Coren <hillelcoren@gmail.com>
2022-06-20 11:47:35 +0300

    Purchase orders

--- lib/utils/web.dart
+++ lib/utils/web.dart
@@ -74,7 +74,8 @@ class WebUtils {
       ..auth = (BrowserAuthOptions()
         //..redirectUri = 'https://invoicing.co/auth/microsoft'
         //..redirectUri = browserUrl
-        ..redirectUri = 'https://staging.invoicing.co/'
+        //..redirectUri = 'https://staging.invoicing.co/'
+        ..redirectUri = 'http://localhost:8080/'
         ..clientId = Config.MICROSOFT_CLIENT_ID);
     final publicClientApp = PublicClientApplication(config);
 
