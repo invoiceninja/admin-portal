3ad4be2efdd31453695312e3232b69bb78edf4ce
Hillel Coren <hillelcoren@gmail.com>
2022-07-04 13:03:14 +0300

    App review reminder

---
+++ lib/ui/app/review_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReviewApp extends StatelessWidget {
+  const ReviewApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
