531b88109e893b9a6a740cbc52a63c20587f3c08
Hillel Coren <hillelcoren@gmail.com>
2022-09-01 12:01:08 +0300

    Google GMS in F-Droid build #478

---
+++ lib/ui/app/pinput.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:invoiceninja_flutter/utils/localization.dart';
+import 'package:pinput/pinput.dart';
+
+class AppPinput extends StatelessWidget {
+  const AppPinput({Key key, this.onCompleted}) : super(key: key);
+
+  final ValueChanged<String> onCompleted;
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalization.of(context);
+
+    return Pinput(
+      onCompleted: onCompleted,
+      autofocus: true,
+      length: 6,
+      showCursor: true,
+      androidSmsAutofillMethod: AndroidSmsAutofillMethod.smsUserConsentApi,
+      validator: (value) =>
+          value.isEmpty ? localization.pleaseEnterACode : null,
+    );
+  }
+}
