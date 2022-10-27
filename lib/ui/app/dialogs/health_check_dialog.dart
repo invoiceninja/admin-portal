061720b02e64712f055cbcc5d75ef13ae7215ecc
Hillel Coren <hillelcoren@gmail.com>
2022-10-27 13:00:41 +0300

    Add debug to health check

--- lib/ui/app/dialogs/health_check_dialog.dart
+++ lib/ui/app/dialogs/health_check_dialog.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 
 // Flutter imports:
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -54,7 +55,9 @@ class _HealthCheckDialogState extends State<HealthCheckDialog> {
     final url = '${credentials.url}/health_check';
 
     webClient.get(url, credentials.token).then((dynamic response) {
-      //print('## response: $response');
+      if (!kReleaseMode) {
+        print('## response: $response');
+      }
       setState(() {
         _response = serializers.deserializeWith(
             HealthCheckResponse.serializer, response);
