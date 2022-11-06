fdc553e1f3f0a7e69865417b384daba5544dd2fb
Hillel Coren <hillelcoren@gmail.com>
2022-11-06 12:05:44 +0200

    Update Flutter version meta data

--- lib/flutter_version.dart
+++ lib/flutter_version.dart
@@ -1,11 +1,11 @@
 const FLUTTER_VERSION = const <String, String>{
-  "frameworkVersion": "3.3.4",
-  "channel": "stable",
-  "repositoryUrl": "git@github.com:flutter/flutter.git",
-  "frameworkRevision": "eb6d86ee27deecba4a83536aa20f366a6044895c",
-  "frameworkCommitDate": "2022-10-04 22:31:45 -0700",
-  "engineRevision": "c08d7d5efc9aa6eb3c30cfb3be6dc09bca5e7631",
-  "dartSdkVersion": "2.18.2",
-  "devToolsVersion": "2.15.0",
-  "flutterRoot": "C:\\Users\\hillel\\Documents\\flutter"
+  'frameworkVersion': '3.3.4',
+  'channel': 'stable',
+  'repositoryUrl': 'git@github.com:flutter/flutter.git',
+  'frameworkRevision': 'eb6d86ee27deecba4a83536aa20f366a6044895c',
+  'frameworkCommitDate': '2022-10-04 22:31:45 -0700',
+  'engineRevision': 'c08d7d5efc9aa6eb3c30cfb3be6dc09bca5e7631',
+  'dartSdkVersion': '2.18.2',
+  'devToolsVersion': '2.15.0',
+  'flutterRoot': 'C:\\Users\\hillel\\Documents\\flutter'
 };
