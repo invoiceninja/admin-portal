2a4172616ce2d60725ffa0a57764d8aa516671ac
Hillel Coren <hillelcoren@gmail.com>
2022-12-05 19:11:53 +0200

    Auto-select bank account if only one

--- lib/data/models/transaction_model.dart
+++ lib/data/models/transaction_model.dart
@@ -70,6 +70,17 @@ abstract class TransactionEntity extends Object
     with BaseEntity
     implements Built<TransactionEntity, TransactionEntityBuilder> {
   factory TransactionEntity({String id, AppState state}) {
+    String bankAccountId = '';
+    if (state != null) {
+      final bankAccounts = state.bankAccountState.list
+          .map((bankAccountId) => state.bankAccountState.map[bankAccountId])
+          .where((bankAccount) => !bankAccount.isDeleted)
+          .toList();
+      if (bankAccounts.length == 1) {
+        bankAccountId = bankAccounts.first.id;
+      }
+    }
+
     return _$TransactionEntity._(
       id: id ?? BaseEntity.nextId,
       isChanged: false,
@@ -80,7 +91,7 @@ abstract class TransactionEntity extends Object
       assignedUserId: '',
       archivedAt: 0,
       amount: 0,
-      bankAccountId: '',
+      bankAccountId: bankAccountId,
       category: '',
       currencyId: state?.company?.currencyId ?? kDefaultCurrencyId,
       date: convertDateTimeToSqlDate(),
