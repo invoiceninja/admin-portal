b8a6b6c7bf1c11300aa66f347d0e26bc3a42f5ac
Hillel Coren <hillelcoren@gmail.com>
2022-12-08 17:10:38 +0200

    Make scrollbar primary on edit expense

--- lib/ui/expense/edit/expense_edit_desktop.dart
+++ lib/ui/expense/edit/expense_edit_desktop.dart
@@ -19,6 +19,7 @@ class ExpenseEditDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScrollableListView(
+      primary: true,
       children: [
         Row(
           crossAxisAlignment: CrossAxisAlignment.start,
