3dd2c0c346c89356230c4e50d6d07f2f441a083d
Hillel Coren <hillelcoren@gmail.com>
2023-01-09 16:43:43 +0200

    Fix filtering by project

--- lib/redux/recurring_invoice/recurring_invoice_selectors.dart
+++ lib/redux/recurring_invoice/recurring_invoice_selectors.dart
@@ -66,6 +66,9 @@ List<String> filteredRecurringInvoicesSelector(
     } else if (filterEntityType == EntityType.group &&
         client.groupId != filterEntityId) {
       return false;
+    } else if (filterEntityType == EntityType.project &&
+        invoice.projectId != filterEntityId) {
+      return false;
     }
 
     if (!invoice.matchesStates(invoiceListState.stateFilters)) {
