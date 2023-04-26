8ae093bdf190f2b39fc186b42a1c7730947e090a
Hillel Coren <hillelcoren@gmail.com>
2023-04-26 08:22:23 +0300

    Fix tax id

--- lib/ui/invoice/edit/invoice_edit_items_desktop.dart
+++ lib/ui/invoice/edit/invoice_edit_items_desktop.dart
@@ -694,7 +694,7 @@ class _InvoiceEditItemsDesktopState extends State<InvoiceEditItemsDesktop> {
                                       ..customValue2 = product.customValue2
                                       ..customValue3 = product.customValue3
                                       ..customValue4 = product.customValue4
-                                      ..taskId = product.taxId
+                                      ..taxId = product.taxId
                                       ..taxName1 =
                                           company.numberOfItemTaxRates >= 1 &&
                                                   product.taxName1.isNotEmpty
