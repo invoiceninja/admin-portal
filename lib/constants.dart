95ae8e2a2e74073061db60f5652804c13feab89e
Hillel Coren <hillelcoren@gmail.com>
2023-06-14 10:07:25 +0300

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ class Constants {
 }
 
 // TODO remove version once #46609 is fixed
-const String kClientVersion = '5.0.119';
+const String kClientVersion = '5.0.120';
 const String kMinServerVersion = '5.0.4';
 
 const String kAppName = 'Invoice Ninja';
