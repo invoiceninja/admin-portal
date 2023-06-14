ef6e7a206be7e62bf28c3ccef4eb2cc23233b6cd
Hillel Coren <hillelcoren@gmail.com>
2023-06-14 23:41:31 +0300

    macOS widgets

--- macos/CompanyIntent/IntentHandler.swift
+++ macos/CompanyIntent/IntentHandler.swift
@@ -9,35 +9,49 @@ import Intents
 import DashboardWidgetExtension
 
 class IntentHandler: INExtension, ConfigurationIntentHandling {
-
+    
     func provideCompanyOptionsCollection(for intent: ConfigurationIntent) async throws -> INObjectCollection<Company> {
         
         // 1
         // Fetch list of top ten crypto from API
         //let assets = try await AssetFetcher.fetchTopTenAssets()
         
-        // 2
-        // Transform `[Asset]` to `[Crypto]`
-        /*
-        let cryptos = assets.map { asset in
-            
-            let crypto = Crypto(
-                identifier: asset.id,
-                display: "\(asset.name) (\(asset.symbol))"
-            )
-            crypto.symbol = asset.symbol
-            crypto.name = asset.name
-            
-            return crypto
+
+        
+        let sharedDefaults = UserDefaults.init(suiteName: "group.com.invoiceninja.app")
+        var exampleData: WidgetData = WidgetData(tokens:[:])
+
+        if sharedDefaults != nil {
+          do {
+            let shared = sharedDefaults!.string(forKey: "widgetData")
+            if shared != nil {
+              let decoder = JSONDecoder()
+              exampleData = try decoder.decode(WidgetData.self, from: shared!.data(using: .utf8)!)
+            }
+          } catch {
+            print(error)
+          }
         }
-        */
+        
+        let companies = exampleData.tokens.keys.map { token in
+            
+            let company = Company(
+                identifier: token,
+                display: exampleData.tokens[token] ?? ""
+            )
+            //company.symbol = asset.symbol
+            //company.name = asset.name
+            
+            return company
+        }
         
         // 3
         // Create a collection with the array of cryptos.
-        let company1 = Company(identifier: "1", display: "Test 1")
-        let company2 = Company(identifier: "2", display: "Test 2")
-
-        let collection = INObjectCollection(items: [company1, company2])
+        //let company1 = Company(identifier: "1", display: "Test 1")
+        //let company2 = Company(identifier: "2", display: "Test 2")
+        //let collection = INObjectCollection(items: [company1, company2])
+        
+        let collection = INObjectCollection(items: companies)
         
         // Return the collections
         return collection
