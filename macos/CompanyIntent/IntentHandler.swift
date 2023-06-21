186c4b972a17c13ec4876766294d26f13e4c0cb5
Hillel Coren <hillelcoren@gmail.com>
2023-06-21 17:55:17 +0300

    macOS widgets

--- macos/CompanyIntent/IntentHandler.swift
+++ macos/CompanyIntent/IntentHandler.swift
@@ -8,7 +8,7 @@
 import Intents
 
 class IntentHandler: INExtension, ConfigurationIntentHandling {
-    
+
     func provideCompanyOptionsCollection(for intent: ConfigurationIntent) async throws -> INObjectCollection<Company> {
               
         let sharedDefaults = UserDefaults.init(suiteName: "group.com.invoiceninja.app")
@@ -32,6 +32,44 @@ class IntentHandler: INExtension, ConfigurationIntentHandling {
         let companies = exampleData.companies.values.map { company in
             
             let company = Company(
+                identifier: company.token,
+                display: company.name
+            )
+            //company.symbol = asset.symbol
+            //company.name = asset.name
+            
+            return company
+        }
+          
+        let collection = INObjectCollection(items: companies)
+        
+        return collection
+        
+    }
+    
+    func provideCurrencyOptionsCollection(for intent: ConfigurationIntent) async throws -> INObjectCollection<Currency> {
+              
+        let sharedDefaults = UserDefaults.init(suiteName: "group.com.invoiceninja.app")
+        var exampleData: WidgetData = WidgetData(url: "", companies: [:])
+
+        if sharedDefaults != nil {
+          do {
+            let shared = sharedDefaults!.string(forKey: "widget_data")
+              
+              print("## Shared: \(shared!)")
+              
+            if shared != nil {
+              let decoder = JSONDecoder()
+              exampleData = try decoder.decode(WidgetData.self, from: shared!.data(using: .utf8)!)
+            }
+          } catch {
+            print(error)
+          }
+        }
+        
+        let companies = exampleData.companies.values.map { company in
+            
+            let company = Currency(
                 identifier: company.token,
                 display: company.name
             )
