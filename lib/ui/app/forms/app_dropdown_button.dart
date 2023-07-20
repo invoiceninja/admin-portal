473c8d8b956ca4c12bd7fb21fd31990e42ee34b5
Hillel Coren <hillelcoren@gmail.com>
2023-07-20 13:44:15 +0300

    Improve auto-taxes

--- lib/ui/app/forms/app_dropdown_button.dart
+++ lib/ui/app/forms/app_dropdown_button.dart
@@ -55,7 +55,7 @@ class AppDropdownButton<T> extends StatelessWidget {
     if (labelText != null) {
       dropDownButton = InputDecorator(
           decoration: InputDecoration(
-            labelText: labelText,
+            labelText: labelText.isEmpty ? null : '',
           ),
           isEmpty: isEmpty && blankLabel == null,
           child: dropDownButton);
