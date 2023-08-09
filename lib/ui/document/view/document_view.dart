c50a6e5bae31c69dc66b97dcff99fa5b4663b49a
Hillel Coren <hillelcoren@gmail.com>
2023-08-10 00:28:03 +0300

    Improve documents

--- lib/ui/document/view/document_view.dart
+++ lib/ui/document/view/document_view.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:invoiceninja_flutter/ui/app/form_card.dart';
+import 'package:invoiceninja_flutter/ui/app/loading_indicator.dart';
 import 'package:invoiceninja_flutter/ui/app/view_scaffold.dart';
 import 'package:invoiceninja_flutter/ui/document/view/document_view_vm.dart';
+import 'package:pinch_zoom/pinch_zoom.dart';
+import 'package:printing/printing.dart';
 
 class DocumentView extends StatefulWidget {
   const DocumentView({
@@ -29,8 +32,24 @@ class _DocumentViewState extends State<DocumentView> {
     return ViewScaffold(
       isFilter: widget.isFilter,
       entity: document,
-      body: FormCard(children: [
-        Text('${document.data?.length ?? ''}'),
+      body: Column(children: [
+        document.data == null
+            ? LoadingIndicator()
+            : SizedBox(
+                height: 600,
+                child: document.isImage
+                    ? PinchZoom(
+                        child: Image.memory(document.data),
+                      )
+                    : PdfPreview(
+                        build: (format) => document.data,
+                        canChangeOrientation: false,
+                        canChangePageFormat: false,
+                        allowPrinting: false,
+                        allowSharing: false,
+                        canDebug: false,
+                      ),
+              )
       ]),
     );
   }
