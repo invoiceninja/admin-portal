cdbdb369cb8b55c46c23a690fecb4ac669bbb49d
Hillel Coren <hillelcoren@gmail.com>
2023-11-01 16:21:13 +0200

    Bump version script

--- bump_version.sh
+++ bump_version.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "Bump version..."
-
 current_version=$(grep "version: 5.0." pubspec.yaml | cut -f2 -d "+" )
-echo "Current Version: $current_version"
-
 new_vesion=$((current_version+1))
-echo "New Version: $new_vesion"
 
-# pubspec.foss.yaml
+echo "Bump version... $current_version => $new_vesion"
+
+sed -i -e "s/version: 5.0.$current_version+$current_version/version: 5.0.$new_vesion+$new_vesion/g" ./pubspec.yaml
+sed -i -e "s/version: 5.0.$current_version+$current_version/version: 5.0.$new_vesion+$new_vesion/g" ./pubspec.foss.yaml
+
+
+
 # pubspec.yaml
+# pubspec.foss.yaml
 # flatpak.yml
 # com.invoiceninja.app.metainfo
 # contants
