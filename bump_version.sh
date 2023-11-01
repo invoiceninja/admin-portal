fb5edc96e4c1bc5c70d20d5c533d4c2fe80111e6
Hillel Coren <hillelcoren@gmail.com>
2023-11-01 18:04:16 +0200

    Flatpak

--- bump_version.sh
+++ bump_version.sh
@@ -8,6 +8,6 @@ echo "Bump version... $current_version => $new_vesion"
 sed -i -e "s/version: 5.0.$current_version+$current_version/version: 5.0.$new_vesion+$new_vesion/g" ./pubspec.yaml
 sed -i -e "s/version: 5.0.$current_version+$current_version/version: 5.0.$new_vesion+$new_vesion/g" ./pubspec.foss.yaml
 sed -i -e "s/v5.0.$current_version/v5.0.$new_vesion/g" ./.github/workflows/flatpak.yml
-sed -i -e 's/<releases>/<releases>\n    <release version="5.0.'$new_vesion'" date="XXXX-XX-XX"\/>/g' ./flatpak/com.invoiceninja.app.metainfo.xml
+sed -i -e 's/<releases>/<releases>\n    <release version="5.0.'$new_vesion'" date="XXXX-XX-XX"\/>/g' ./flatpak/com.invoiceninja.InvoiceNinja.metainfo.xml
 sed -i -e "s/kClientVersion = '5.0.$current_version'/kClientVersion = '5.0.$new_vesion'/g" ./lib/constants.dart
 sed -i -e "s/version: '5.0.$current_version'/version: '5.0.$new_vesion'/g" ./snap/snapcraft.yaml
