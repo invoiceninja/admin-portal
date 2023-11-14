e879c3c4b1a5b7b6b759264e4c3c782ce7db4276
Hillel Coren <hillelcoren@gmail.com>
2023-11-14 10:50:28 +0200

    Force build

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,7 +8,7 @@ const String kClientVersion = '5.0.140';
 const String kMinServerVersion = '5.0.4';
 
 const String kAppName = 'Invoice Ninja';
-const String kSiteUrl = 'https://invoiceninja.com';
+const String kSiteUrl = 'https://invoiceninja.com/';
 const String kAppProductionUrl = 'https://invoicing.co';
 const String kAppStagingUrl = 'https://staging.invoicing.co';
 const String kAppStagingNetUrl = 'https://invoiceninja.net';
