f8198033bbb1b86acf4903eaa88742a8a0aee7fa
Hillel Coren <hillelcoren@gmail.com>
2024-01-11 13:19:31 +0200

    Add padding

--- lib/ui/settings/generated_numbers.dart
+++ lib/ui/settings/generated_numbers.dart
@@ -557,7 +557,10 @@ class _EntityNumberSettingsState extends State<EntityNumberSettings> {
           padding:
               const EdgeInsets.only(left: 16, top: 20, right: 16, bottom: 8),
           child: OutlinedButton(
-            child: Text(localization.viewDateFormats.toUpperCase()),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(localization.viewDateFormats.toUpperCase()),
+            ),
             onPressed: () => launchUrl(
                 Uri.parse('https://www.php.net/manual/en/datetime.format.php')),
           ),
