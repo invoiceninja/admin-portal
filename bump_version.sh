0573fd2efa2a4d1e9fecd06c80ad99c581b45812
Hillel Coren <hillelcoren@gmail.com>
2024-05-26 19:30:23 +0300

    Update version file when bumping version

--- bump_version.sh
+++ bump_version.sh
@@ -12,3 +12,10 @@ sed -i -e "s/v5.0.$current_version/v5.0.$new_vesion/g" ./.github/workflows/flatp
 sed -i -e 's/<releases>/<releases>\n    <release version="5.0.'$new_vesion'" date="'$date_today'"\/>/g' ./flatpak/com.invoiceninja.InvoiceNinja.metainfo.xml
 sed -i -e "s/kClientVersion = '5.0.$current_version'/kClientVersion = '5.0.$new_vesion'/g" ./lib/constants.dart
 sed -i -e "s/version: '5.0.$current_version'/version: '5.0.$new_vesion'/g" ./snap/snapcraft.yaml
+
+rm lib/flutter_version.dart
+echo "const FLUTTER_VERSION = const <String, String>" > lib/flutter_version.dart
+flutter --version --machine >> lib/flutter_version.dart
+echo ";" >> lib/flutter_version.dart
+sed -i "y/\"/'/" lib/flutter_version.dart
+flutter dartfmt lib
