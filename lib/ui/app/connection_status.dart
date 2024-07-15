4c66c97ef03a06ce3e93b5f08d921a8721ec4848
Souradip Mookerjee <sm2030@cam.ac.uk>
2024-07-15 09:55:10 +0100

    back online green banner

--- lib/ui/app/connection_status.dart
+++ lib/ui/app/connection_status.dart
@@ -4,6 +4,7 @@ import 'package:invoiceninja_flutter/utils/connection_status.dart';
 
 class ConnStatusBannerState extends State<ConnStatusBanner> {
   bool isOffline = false;
+  bool shouldShowOnlineBanner = false;
 
   @override
   void initState() {
@@ -15,10 +16,22 @@ class ConnStatusBannerState extends State<ConnStatusBanner> {
 
     setState(() {
       isOffline = !connectionStatus.hasConnection;
+      shouldShowOnlineBanner = false;
     });
   }
 
   void connectionChanged(dynamic hasConnection) {
+    if (isOffline && hasConnection) {
+      setState(() {
+        shouldShowOnlineBanner = true;
+      });
+
+      Future.delayed(Duration(seconds: 2), () {
+        setState(() {
+          shouldShowOnlineBanner = false;
+        });
+      });
+    }
     setState(() {
       isOffline = !hasConnection;
     });
@@ -26,9 +39,9 @@ class ConnStatusBannerState extends State<ConnStatusBanner> {
 
   @override
   Widget build(BuildContext ctxt) {
-    return isOffline
+    return shouldShowOnlineBanner
         ? Container(
-            color: Colors.red,
+            color: Colors.green,
             child: Padding(
               padding: EdgeInsets.only(
                   left: 5.0, right: 5.0, top: 10.0, bottom: 10.0),
@@ -37,7 +50,7 @@ class ConnStatusBannerState extends State<ConnStatusBanner> {
                   Expanded(
                     child: Center(
                       child: Text(
-                        'No Internet Connection - showing cached data',
+                        'Back online!',
                         style: TextStyle(
                             color: Colors.white,
                             fontSize: 12,
@@ -49,7 +62,30 @@ class ConnStatusBannerState extends State<ConnStatusBanner> {
               ),
             ),
           )
-        : Container();
+        : isOffline
+            ? Container(
+                color: Colors.red,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 5.0, right: 5.0, top: 10.0, bottom: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            'No Internet Connection - showing cached data',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontFamily: "Roboto"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            : Container();
   }
 }
 
