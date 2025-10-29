ef138a35f618df5da6d4ac6338bcf28db8021229
Hillel Coren <hillelcoren@gmail.com>
2025-10-29 12:50:09 +0200

    Fix dart warnings

--- lib/flutter_version.dart
+++ lib/flutter_version.dart
@@ -1,15 +1,15 @@
 const FLUTTER_VERSION = const <String, String>{
-  "frameworkVersion": "3.35.4",
-  "channel": "stable",
-  "repositoryUrl": "https://github.com/flutter/flutter.git",
-  "frameworkRevision": "d693b4b9dbac2acd4477aea4555ca6dcbea44ba2",
-  "frameworkCommitDate": "2025-09-16 14:27:41 +0000",
-  "engineRevision": "c29809135135e262a912cf583b2c90deb9ded610",
-  "engineCommitDate": "2025-09-15 14:04:24.000Z",
-  "engineContentHash": "feee8ee8fb8b975dd9990f86d3bda11e6e75faf3",
-  "engineBuildDate": "2025-09-15 17:37:23.590",
-  "dartSdkVersion": "3.9.2",
-  "devToolsVersion": "2.48.0",
-  "flutterVersion": "3.35.4",
-  "flutterRoot": "/Users/hillel/Code/flutter"
+  'frameworkVersion': '3.35.4',
+  'channel': 'stable',
+  'repositoryUrl': 'https://github.com/flutter/flutter.git',
+  'frameworkRevision': 'd693b4b9dbac2acd4477aea4555ca6dcbea44ba2',
+  'frameworkCommitDate': '2025-09-16 14:27:41 +0000',
+  'engineRevision': 'c29809135135e262a912cf583b2c90deb9ded610',
+  'engineCommitDate': '2025-09-15 14:04:24.000Z',
+  'engineContentHash': 'feee8ee8fb8b975dd9990f86d3bda11e6e75faf3',
+  'engineBuildDate': '2025-09-15 17:37:23.590',
+  'dartSdkVersion': '3.9.2',
+  'devToolsVersion': '2.48.0',
+  'flutterVersion': '3.35.4',
+  'flutterRoot': '/Users/hillel/Code/flutter'
 };
