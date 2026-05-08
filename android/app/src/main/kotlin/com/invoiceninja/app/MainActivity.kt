47660c57e52d07cfdab583ed9daa7860e1ab0d55
Hillel Coren <hillelcoren@gmail.com>
2026-05-08 10:56:52 +0300

    Update Android

--- android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.invoiceninja.app
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-}
+class MainActivity : FlutterActivity()
