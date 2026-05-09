9a108abb7bb4a7ae7273962b35960adde0cb3301
Hillel Coren <hillelcoren@gmail.com>
2026-05-09 23:16:54 +0300

    Fix for bio-auth

--- android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/invoiceninja/app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.invoiceninja.app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
